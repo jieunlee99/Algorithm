f92c982efab90a0dc798cde934a602a041b5d026
jieunlee99 <105425346+jieunlee99@users.noreply.github.com>
2024-10-25 14:54:07 +0900

    [Silver III] Title: 분수 합, Time: 68 ms, Memory: 11524 KB -BaekjoonHub

--- 백준/Silver/1735. 분수 합/분수 합.java
+++ 백준/Silver/1735. 분수 합/분수 합.java
@@ -1,38 +1,35 @@
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        StringTokenizer st = new StringTokenizer(br.readLine());
         int a1 = Integer.parseInt(st.nextToken());
         int b1 = Integer.parseInt(st.nextToken());
 
-        st = new StringTokenizer(br.readLine(), " ");
+        st = new StringTokenizer(br.readLine());
         int a2 = Integer.parseInt(st.nextToken());
         int b2 = Integer.parseInt(st.nextToken());
 
-        int down = lcm(b1, b2); // 합 분모
-        int up = down/b1*a1+down/b2*a2; // 합 분자
+        int down = lcm(b1, b2);
+        int up = down/b1*a1+ down/b2*a2;
         int gcd = gcd(down, up);
-        System.out.println(up/gcd+" "+down/gcd);
-    }
 
-    static int gcd(int a, int b) {
-        int r;
-        while(b!=0) {
-            r = a%b;
-            a = b;
-            b = r;
-        }
-        return a;
+        System.out.println(up/gcd+" "+down/gcd);
     }
 
     static int lcm(int a, int b) {
         return (a*b)/gcd(a, b);
+    }
+
+    static int gcd(int a, int b) {
+        if(a%b == 0) {
+            return b;
+        }
+        return  gcd(b, a%b);
     }
 }
