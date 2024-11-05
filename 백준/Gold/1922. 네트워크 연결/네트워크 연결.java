327093542332774d0d590adfa347a61f63e091a2
jieunlee99 <105425346+jieunlee99@users.noreply.github.com>
2024-11-05 15:49:34 +0900

    [Gold IV] Title: 네트워크 연결, Time: 432 ms, Memory: 46916 KB -BaekjoonHub

--- 백준/Gold/1922. 네트워크 연결/네트워크 연결.java
+++ 백준/Gold/1922. 네트워크 연결/네트워크 연결.java
@@ -1,103 +1,85 @@
 
-import java.io.BufferedReader;
-//import java.io.FileInputStream;
-//import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
 public class Main {
 
+	static StringTokenizer st;
 	static int N, M;
 	static int[] parent;
-	static Edge[] edges;
+	static PriorityQueue<Edge> queue;
 
-	static int sum = 0;
-
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		// TODO Auto-generated method stub
-//		System.setIn(new FileInputStream("src/DAY08/P1922/input.txt"));
+	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
 		N = Integer.parseInt(br.readLine()); // 컴퓨터의 수
 		M = Integer.parseInt(br.readLine()); // 연결할 수 있는 선의 수
 
 		parent = new int[N + 1];
-		for (int n = 1; n <= N; n++) {
-			parent[n] = n;
+		for (int i = 1; i <= N; i++) {
+			parent[i] = i;
 		}
+		
+		queue = new PriorityQueue<>();
 
-		edges = new Edge[M];
-
-		StringTokenizer st;
-		int a, b, c;
-
-		for (int m = 0; m < M; m++) {
+		for (int i = 1; i <= M; i++) {
 			st = new StringTokenizer(br.readLine());
-			// a와 b를 연결하는 비용이 c
-			a = Integer.parseInt(st.nextToken());
-			b = Integer.parseInt(st.nextToken());
-			c = Integer.parseInt(st.nextToken());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			int c = Integer.parseInt(st.nextToken());
 
-			edges[m] = new Edge(a, b, c);
+			queue.offer(new Edge(a, b, c));
 		}
 
-		Arrays.sort(edges, Comparator.comparing(Edge::getCost));
-//		System.out.println(Arrays.toString(edges));
-
-		for (int m = 0; m < M; m++) {
-			// 같은 set인지 검사
-			Edge current = edges[m];
-			if (find(current.from) != find(current.to)) {
-				union(current.from, current.to);
-				sum += current.cost;
-
+		int weightSum = 0;
+		
+		while(!queue.isEmpty()) {
+			Edge current = queue.poll();
+			
+			if(find(current.v1) != find(current.v2)) {
+				union(current.v1, current.v2);
+				weightSum += current.weight;
 			}
 		}
-
-		System.out.println(sum);
-	}
-
-	static int find(int a) {
-		if (parent[a] == a)
-			return a;
-		else
-			return parent[a] = find(parent[a]);
+		
+		System.out.println(weightSum);
 	}
 
 	static void union(int a, int b) {
 		int aRoot = find(a);
 		int bRoot = find(b);
-		parent[aRoot] = bRoot;
+
+		if (aRoot < bRoot) {
+			parent[bRoot] = aRoot;
+		} else {
+			parent[aRoot] = bRoot;
+		}
 	}
 
+	static int find(int n) {
+		if (parent[n] == n) {
+			return n;
+		} else {
+			return parent[n] = find(parent[n]);
+		}
+	}
 }
 
-class Edge {
-	int from, to, cost;
+class Edge implements Comparable<Edge> {
 
-	public int getFrom() {
-		return from;
-	}
+	int v1, v2;
+	int weight;
 
-	public int getTo() {
-		return to;
-	}
-
-	public int getCost() {
-		return cost;
-	}
-
-	public Edge(int from, int to, int cost) {
-		this.from = from;
-		this.to = to;
-		this.cost = cost;
+	public Edge(int v1, int v2, int weight) {
+		this.v1 = v1;
+		this.v2 = v2;
+		this.weight = weight;
 	}
 
 	@Override
-	public String toString() {
-		return "Edge [from=" + from + ", to=" + to + ", cost=" + cost + "]";
+	public int compareTo(Edge edge) {
+		// TODO Auto-generated method stub
+		return weight - edge.weight;
 	}
+
 }
