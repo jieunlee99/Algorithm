1cda94f5a106396a70d0c920fd615ab9baf63db2
jieunlee99 <105425346+jieunlee99@users.noreply.github.com>
2024-11-18 15:08:23 +0900

    [Bronze III] Title: 별 찍기 - 4, Time: 216 ms, Memory: 15788 KB -BaekjoonHub

---
+++ 백준/Bronze/2441. 별 찍기 － 4/별 찍기 － 4.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		for(int i=0; i<N; i++) {
+			for(int j=0; j<i; j++) {
+				System.out.print(" ");
+			}
+			
+			for(int j=N; j>i; j--) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
