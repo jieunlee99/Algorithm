d0314f7f0bde3117e51f9784d776dee3d9787ba8
jieunlee99 <105425346+jieunlee99@users.noreply.github.com>
2024-11-23 16:50:37 +0900

    [Silver V] Title: 덩치, Time: 104 ms, Memory: 14136 KB -BaekjoonHub

---
+++ 백준/Silver/7568. 덩치/덩치.java
@@ -0,0 +1,50 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	
+	static int N;
+	static Person[] people; 
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		N = Integer.parseInt(br.readLine());
+		
+		people = new Person[N+1];
+		
+		for(int i=0; i<N; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int x = Integer.parseInt(st.nextToken());
+			int y = Integer.parseInt(st.nextToken());
+			people[i] = new Person(x, y);
+		}
+	
+		StringBuilder sb = new StringBuilder();
+		for(int i=0; i<N; i++) {
+			sb.append(solution(people[i])).append(" ");
+		}
+		System.out.print(sb.toString());
+	}
+	
+	static int solution(Person p) {
+		int cnt = 1;
+		
+		for(int i=0; i<N; i++)  {
+			if(people[i].weight > p.weight && people[i].height > p.height) {
+				cnt++;
+			}
+		}
+		
+		return cnt;
+	}
+}
+
+class Person{
+	int height, weight;
+	
+	public Person(int height, int weight) {
+		this.height = height;
+		this.weight = weight;
+	}
+}
