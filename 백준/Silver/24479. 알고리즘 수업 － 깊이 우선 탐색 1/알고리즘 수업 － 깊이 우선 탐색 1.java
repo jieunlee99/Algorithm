fa9d059d0f3de2d87d8b87f527b79ea0f3378eea
jieunlee99 <105425346+jieunlee99@users.noreply.github.com>
2024-11-29 22:45:04 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 1, Time: 896 ms, Memory: 103220 KB -BaekjoonHub

---
+++ 백준/Silver/24479. 알고리즘 수업 － 깊이 우선 탐색 1/알고리즘 수업 － 깊이 우선 탐색 1.java
@@ -0,0 +1,53 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static int N, M, R;
+    static ArrayList<Integer>[] adjList;
+    static int[] dp;
+    static int cnt = 1;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        R = Integer.parseInt(st.nextToken());
+
+        adjList = new ArrayList[N + 1];
+        for (int i = 1; i <= N; i++) {
+            adjList[i] = new ArrayList<>();
+        }
+
+        dp = new int[N + 1];
+
+        for (int m = 0; m < M; m++) {
+            st = new StringTokenizer(br.readLine());
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            adjList[u].add(v);
+            adjList[v].add(u);
+        }
+
+        dfs(R);
+
+        for(int i=1; i<=N; i++) {
+            sb.append(dp[i]).append("\n");
+        }
+        System.out.println(sb);
+    }
+
+    static void dfs(int current) {
+        dp[current] = cnt++;
+
+        Collections.sort(adjList[current]);
+        for(int next : adjList[current]) {
+            if(dp[next] == 0) {
+                dfs(next);
+            }
+        }
+    }
+}
