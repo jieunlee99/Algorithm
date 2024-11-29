a30b829e3d139a62c9d6682ec11d619fc3bcf647
jieunlee99 <105425346+jieunlee99@users.noreply.github.com>
2024-11-29 22:51:37 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 1, Time: 892 ms, Memory: 91436 KB -BaekjoonHub

---
+++ 백준/Silver/24444. 알고리즘 수업 － 너비 우선 탐색 1/알고리즘 수업 － 너비 우선 탐색 1.java
@@ -0,0 +1,59 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static int N, M, R;
+    static ArrayList<Integer>[] adjList;
+    static int[] dp;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        R = Integer.parseInt(st.nextToken());
+
+        adjList = new ArrayList[N + 1];
+        for (int i = 1; i <= N; i++) {
+            adjList[i] = new ArrayList<>();
+        }
+
+        dp = new int[N + 1];
+
+        for (int m = 0; m < M; m++) {
+            st = new StringTokenizer(br.readLine());
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            adjList[u].add(v);
+            adjList[v].add(u);
+        }
+
+        int cnt = 1;
+
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(R);
+        dp[R] = cnt++;
+
+        while (!queue.isEmpty()) {
+            int current = queue.poll();
+
+            Collections.sort(adjList[current]);
+            for (int next : adjList[current]) {
+                if (dp[next] == 0) {
+                    dp[next] = cnt++;
+                    queue.offer(next);
+                }
+            }
+        }
+
+        for (int i = 1; i <= N; i++) {
+            sb.append(dp[i]).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+}
+
