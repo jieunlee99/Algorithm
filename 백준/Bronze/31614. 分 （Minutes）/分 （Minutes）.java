50ab785d7afdc4b77dfc383df0925d8d44ac24b2
jieunlee99 <105425346+jieunlee99@users.noreply.github.com>
2025-03-08 20:39:33 +0900

    [Bronze V] Title: 分 (Minutes), Time: 104 ms, Memory: 14060 KB -BaekjoonHub

---
+++ 백준/Bronze/31614. 分 （Minutes）/分 （Minutes）.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int H = Integer.parseInt(br.readLine());
+		int M = Integer.parseInt(br.readLine());
+
+		System.out.println(H * 60 + M);
+	}
+
+}
