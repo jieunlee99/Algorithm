441f49dd2d4d4aa936bedded7236c002e923bc9d
jieunlee99 <105425346+jieunlee99@users.noreply.github.com>
2025-03-14 22:03:13 +0900

    [Bronze IV] Title: 특식 배부, Time: 100 ms, Memory: 14152 KB -BaekjoonHub

---
+++ 백준/Bronze/27110. 특식 배부/특식 배부.java
@@ -0,0 +1,27 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int A = Integer.parseInt(st.nextToken());
+		int B = Integer.parseInt(st.nextToken());
+		int C = Integer.parseInt(st.nextToken());
+
+		
+		A = Math.min(N, A);
+		B = Math.min(N, B);
+		C = Math.min(N, C);
+		
+		System.out.println(A+B+C);
+	}
+
+}
