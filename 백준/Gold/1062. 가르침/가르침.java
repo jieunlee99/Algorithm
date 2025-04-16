d7c9cb1b16e65c89f6f865a858d0688127157201
jieunlee99 <105425346+jieunlee99@users.noreply.github.com>
2025-04-16 17:23:19 +0900

    [Gold IV] Title: 가르침, Time: 332 ms, Memory: 16056 KB -BaekjoonHub

--- 백준/Gold/1062. 가르침/가르침.java
+++ 백준/Gold/1062. 가르침/가르침.java
@@ -1,90 +1,74 @@
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.util.Scanner;
 
-// 1. 문제 파악
-// 2. 요약 : 입력, 출력, 제한 ...
-// 3. 그림
-// 4. 1~6단계 작성 후 그림과 비교
-// 5. 코딩
+import java.io.*;
+import java.util.*;
 
 public class Main {
-	
-	static int n, k; // 대문자로
+
+	static int N, K;
 	static boolean[] isVisited = new boolean[26];
 	static String[] words;
 	static int max = 0;
-	static int cnt;
 
-	public static void main(String[] args) throws FileNotFoundException {
-		// TODO Auto-generated method stub
-		
-		Scanner sc = new Scanner(System.in);
-		
-		n = sc.nextInt(); // 단어 개수
-		k = sc.nextInt(); // 알고 있는 알파벳 개수
-		
-		sc.nextLine();
-		
-		words = new String[n];
-		
-		for(int i=0; i<n; i++) {
-			words[i]=sc.nextLine();
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		K = Integer.parseInt(st.nextToken());
+
+		words = new String[N];
+		for (int i = 0; i < N; i++) {
+			words[i] = br.readLine();
 		}
-		
-		for(int i=0;i<26;i++) {
-			if(isVisited[i] == false) {
-				dfs(i);
-			}
+
+		if (K < 5) {
+			System.out.println(0);
+			return;
 		}
-		
+
+		// 기본 알파벳 체크 (a, n, t, i, c)
+		isVisited['a' - 'a'] = true;
+		isVisited['n' - 'a'] = true;
+		isVisited['t' - 'a'] = true;
+		isVisited['i' - 'a'] = true;
+		isVisited['c' - 'a'] = true;
+
+		dfs(0, 5); // 시작 인덱스, 현재까지 선택한 알파벳 수
+
 		System.out.println(max);
 	}
-	
-	static int countWords() {
-		int count = 0;
-		
-		for(int i=0; i<n; i++) {
-			boolean isPossible = true;
-			String word= words[i];
-			for(int j=0; j<word.length(); j++) {
-				if(isVisited[word.charAt(j)-'a']==false) {
-					isPossible = false;
+
+	static void dfs(int index, int count) {
+		if (count == K) {
+			max = Math.max(max, countReadableWords());
+			return;
+		}
+
+		for (int i = index; i < 26; i++) {
+			if (!isVisited[i]) {
+				isVisited[i] = true;
+				dfs(i + 1, count + 1);
+				isVisited[i] = false;
+			}
+		}
+	}
+
+	static int countReadableWords() {
+		int readable = 0;
+		for (String word : words) {
+			boolean canRead = true;
+			for (int i = 0; i < word.length(); i++) {
+				char ch = word.charAt(i);
+				if (!isVisited[ch - 'a']) {
+					canRead = false;
 					break;
 				}
 			}
-			if(isPossible== true) {
-				count++;
+			if (canRead) {
+				readable++;
 			}
 		}
-		
-		return count;
-	}
-
-	static void dfs(int depth) {
-		
-		// 1. 체크인
-		isVisited[depth]=true;
-		cnt++;
-		
-		// 2. 목적지인가?
-		if(cnt == k) {
-			//  읽을 수 있는 단어 개수 파악
-			max = Math.max(countWords(), max);
-			
-		} else {
-			// 3. 연결된 곳을 순회
-			for(int next=depth+1; next<26; next++) {
-				// 4. 갈 수 있는가?
-				if(isVisited[next]==false) {
-					// 5. 간다.
-					dfs(next);
-				}
-			}
-		}
-		
-		// 6. 체크아웃
-		isVisited[depth]=false;
-		cnt--;
+		return readable;
 	}
 }
