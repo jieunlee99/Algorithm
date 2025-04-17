16c49df5e3e807469dfadc9aee752466a1e35fb7
jieunlee99 <105425346+jieunlee99@users.noreply.github.com>
2025-04-17 15:18:44 +0900

    [Gold IV] Title: 부분합, Time: 248 ms, Memory: 23824 KB -BaekjoonHub

--- 백준/Gold/1806. 부분합/부분합.java
+++ 백준/Gold/1806. 부분합/부분합.java
@@ -1,45 +1,36 @@
 
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    static int N, S; // 수열의 길이, 합
-    static int[] nums; // 수열
-    static int min = Integer.MAX_VALUE;
 
-    public static void main(String[] args) throws Exception {
-        // System.setIn(new FileInputStream("src/P1806/input.txt"));
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        S = Integer.parseInt(st.nextToken());
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int S = Integer.parseInt(st.nextToken());
 
-        nums = new int[N];
+		int[] nums = new int[N];
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			nums[i] = Integer.parseInt(st.nextToken());
+		}
 
-        st = new StringTokenizer(br.readLine());
-        for(int i=0; i<N; i++) {
-            nums[i] = Integer.parseInt(st.nextToken());
-        }
+		int answer = Integer.MAX_VALUE;
 
-        int sum = 0;
-        int low = 0, high = 0;
+		int sum = 0;
+		int low = 0, high = 0;
 
-        while(high < N) {
-            sum += nums[high++];
+		while (high < N) {
+			sum += nums[high++];
 
-            while(sum >= S) {
-                min = Math.min(min, high-low);
-                sum -= nums[low++];
-            }
-        }
+			while (sum >= S) {
+				answer = Math.min(answer, high - low);
+				sum -= nums[low++];
+			}
+		}
 
-        if(min == Integer.MAX_VALUE) {
-            System.out.println(0);
-        } else {
-            System.out.println(min);
-        }
-    }
+		System.out.println(answer == Integer.MAX_VALUE ? 0 : answer);
+	}
 }
