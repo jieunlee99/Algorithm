a4276da10ef637aef54352ede51ed53810057653
jieunlee99 <105425346+jieunlee99@users.noreply.github.com>
2025-08-08 16:03:12 +0900

    [Silver I] Title: 후보 추천하기, Time: 132 ms, Memory: 16036 KB -BaekjoonHub

--- 백준/Silver/1713. 후보 추천하기/후보 추천하기.java
+++ 백준/Silver/1713. 후보 추천하기/후보 추천하기.java
@@ -1,116 +1,69 @@
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-	
-	static Person[] people = new Person[101];
-	static ArrayList<Person> frames = new ArrayList<>();
-	static int N, G;
+
+	static Student[] students = new Student[101];
+	static ArrayList<Student> frames = new ArrayList<>();
+	static int N, K;
 	static int timestamp = 0;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		N = sc.nextInt();
-		G = sc.nextInt(); 
-		
-		for(int i=0; i<G; i++) {
-			int num = sc.nextInt();
-			
-			if(people[num] == null) {
-				people[num] = new Person(num, 0, timestamp++);
+		N = Integer.parseInt(br.readLine());
+		K = Integer.parseInt(br.readLine());
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < K; i++) {
+			int num = Integer.parseInt(st.nextToken());
+
+			// 처음 추천 받는 학생이라면
+			if (students[num] == null) {
+				students[num] = new Student(num, 0, timestamp);
 			}
-			
-			// 사진틀 안에 학생이 없을 때
-			if(people[num].count == 0) {
-				if(frames.size() >= N) {
-					// 추천수 작고 타임스탬프 작은거 삭제
-					// 둘다 내림차순으로 정렬한 뒤 가장 뒷순위인 학생 삭제
-					Collections.sort(frames, new Comparator<Person>() {
 
-						@Override
-						public int compare(Person p1,Person p2) {
-							// TODO Auto-generated method stub
-							int cnt = p2.count - p1.count;
-							if(cnt ==  0) {
-								return p2.timestamp -p1.timestamp;
-							}
-							return cnt;
-						}
-					});
-					
-					// 삭제되는 학생 번호 저장
-					int temp = frames.remove(N-1).num;
-					people[temp].count = 0;
+			if (students[num].cnt == 0) {
+				// 사진틀이 부족하면 정렬하고 가장 앞의 학생 제거
+				if (frames.size() >= N) {
+					frames.sort(new RecommendComparator());
+					Student removed = frames.remove(0);
+					students[removed.num].cnt = 0; // 초기화
 				}
-				
-				people[num].count += 1;
-				people[num].timestamp = timestamp++;
-				frames.add(people[num]);
+				students[num].cnt = 1;
+				students[num].timestamp = timestamp++;
+				frames.add(students[num]);
+			} else {
+				students[num].cnt++;
 			}
-			// 사진틀 안에 학생이 있을 때
-			else {
-				people[num].count+= 1;
-			}
-			
-//			for(int j=0; j<frames.size(); j++) {
-//				System.out.print(frames.get(j).num+" ");
-//			}
-//			System.out.println();
 		}
-		
-		Collections.sort(frames, Comparator.comparing(Person::getNum));
-		for(int i=0; i<frames.size(); i++) {
-			System.out.print(frames.get(i).num+" ");
+
+		// 번호순 출력
+		frames.sort(Comparator.comparingInt(s -> s.num));
+		for (Student frame : frames) {
+			System.out.print(frame.num + " ");
 		}
-		System.out.println();
-		
 	}
 
+	static class RecommendComparator implements Comparator<Student> {
+		@Override
+		public int compare(Student s1, Student s2) {
+			if (s1.cnt != s2.cnt) {
+				return s1.cnt - s2.cnt; // 추천 수 오름차순
+			}
+			return s1.timestamp - s2.timestamp; // 시간 오름차순
+		}
+	}
 }
 
-class Person implements Comparable<Person>{
+class Student {
+	int num;
+	int cnt;
+	int timestamp;
 
-	int num; // 학생 번호
-	int count; // 추천 수 
-	int timestamp; // 추천받은 시간
-	
-	public Person(int num, int count, int timestamp) {
+	public Student(int num, int cnt, int timestamp) {
 		this.num = num;
-		this.count = count;
+		this.cnt = cnt;
 		this.timestamp = timestamp;
-	}
-	
-	public int getNum() {
-		return num;
-	}
-
-	public void setNum(int num) {
-		this.num = num;
-	}
-
-	public int getCount() {
-		return count;
-	}
-
-	public void setCount(int count) {
-		this.count = count;
-	}
-
-	public int getTimestamp() {
-		return timestamp;
-	}
-
-	public void setTimestamp(int timestamp) {
-		this.timestamp = timestamp;
-	}
-
-	@Override
-	public int compareTo(Person p) {
-		// TODO Auto-generated method stub
-		return num-p.num;
 	}
 }
