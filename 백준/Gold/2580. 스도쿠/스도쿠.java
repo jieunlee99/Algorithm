144dfd4d0cac94999d8e7ec5b0c5eb290f2e302f
jieunlee99 <105425346+jieunlee99@users.noreply.github.com>
2025-08-12 17:35:39 +0900

    [Gold IV] Title: 스도쿠, Time: 460 ms, Memory: 24516 KB -BaekjoonHub

--- 백준/Gold/2580. 스도쿠/스도쿠.java
+++ 백준/Gold/2580. 스도쿠/스도쿠.java
@@ -1,99 +1,86 @@
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
 
-    static int[][] sudoku = new int[9][9];
+	static int[][] sudoku = new int[9][9];
 
-    public static void main(String[] args) throws Exception {
-        // System.setIn(new FileInputStream("src/P2580/input.txt"));
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
 
-        for(int i=0; i<9; i++) {
-            st = new StringTokenizer(br.readLine());
-            for(int j=0; j<9; j++) {
-                sudoku[i][j] = Integer.parseInt(st.nextToken());
-            }
-        }
+		for (int i = 0; i < 9; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < 9; j++) {
+				sudoku[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
 
-        backtracking(0,0);
-    }
+		backtracking(0, 0); 
+	}
 
-    public static void backtracking(int row, int col) {
+	static void backtracking(int row, int col) {
+		if (col == 9) {
+			backtracking(row + 1, 0);
+			return;
+		}
 
-        // 한 행 끝나면 다음 행 검사
-        if(col == 9) {
-            backtracking(row+1, 0);
-            return;
-        }
+		// 2. 목적지인가?
+		if (row == 9) {
+			StringBuilder sb = new StringBuilder();
+			for (int i = 0; i < 9; i++) {
+				for (int j = 0; j < 9; j++) {
+					sb.append(sudoku[i][j]).append(" ");
+				}
+				sb.append("\n");
+			}
+			System.out.print(sb);
+			System.exit(0);
+		}
 
-        // 2. 목적지라면?
-        // 모든 행 검사 후 결과물 출력
-        if(row == 9) {
-            StringBuilder sb = new StringBuilder();
-            for(int i=0; i<9; i++) {
-                for(int j=0; j<9; j++) {
-                    sb.append(sudoku[i][j]).append(" ");
-                }
-                sb.append("\n");
-            }
+		// 빈 칸이 있다면 1~9까지 넣어봄
+		if (sudoku[row][col] == 0) {
+			// 3. 순회
+			for (int i = 1; i <= 9; i++) {
+				// 4. 갈 수 있는가
+				if (isValid(row, col, i)) {
+					// 5. 간다
+					sudoku[row][col] = i;
+					backtracking(row, col + 1);
+				}
+			}
 
-            System.out.println(sb);
+			// 6. 체크아웃
+			sudoku[row][col] = 0;
+			return;
+		}
 
-            // 여러 결과값 중에 하나만 출력하면 되기 때문
-            System.exit(0);
-        }
+		// 해당 열에 빈 칸이 없을 때
+		backtracking(row, col + 1);
+	}
 
-        if(sudoku[row][col] == 0) {
-            // 3. 순회
-            for(int i=1; i<=9; i++) {
-                // 4. 갈 수 있다면
-                if(isValid(row, col, i)) {
-                    // 5. 간다.
-                    sudoku[row][col] = i;
-                    backtracking(row, col+1);
-                }
-            }
+	static boolean isValid(int row, int col, int value) {
 
-            // 6. 체크 아웃
-            sudoku[row][col] = 0;
-            return;
-        }
+		// 같은 행, 열 검사
+		for (int i = 0; i < 9; i++) {
+			if (sudoku[row][i] == value || sudoku[i][col] == value) {
+				return false;
+			}
+		}
 
-        backtracking(row, col+1);
-    }
+		// 3*3 검사 (0, 3, 6으로 만들기)
+		int tx = (row / 3) * 3;
+		int ty = (col / 3) * 3;
 
-    // 같은 행 검사
-    // 같은 열 검사
-    // 3*3 검사
+		for (int i = tx; i < tx + 3; i++) {
+			for (int j = ty; j < ty + 3; j++) {
+				if (sudoku[i][j] == value) {
+					return false;
+				}
+			}
+		}
 
-    public static boolean isValid(int row, int col, int value) {
-
-        // 0, 3, 6으로 만들어 주기
-        int ty = (row/3)*3;
-        int tx = (col/3)*3;
-
-        for(int i=0; i<9; i++) {
-            if(sudoku[row][i] == value) {
-                return false;
-            }
-            if(sudoku[i][col] == value) {
-                return false;
-            }
-        }
-
-        for(int i=ty; i<ty+3; i++) {
-            for(int j=tx; j<tx+3; j++) {
-                if(sudoku[i][j] == value) {
-                    return false;
-                }
-            }
-        }
-
-        return true;
-    }
+		return true;
+	}
 
 }
