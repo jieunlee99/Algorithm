ee9fa147570ee192108728475aac42d91a6cd7ff
jieunlee99 <105425346+jieunlee99@users.noreply.github.com>
2025-08-20 13:40:20 +0900

    [Silver III] Title: 게임, Time: 104 ms, Memory: 14160 KB -BaekjoonHub

--- 백준/Silver/1072. 게임/게임.java
+++ 백준/Silver/1072. 게임/게임.java
@@ -1,45 +1,39 @@
+// 이분 탐색
 
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
 
-    public static void main(String[] args) throws Exception {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        // System.setIn(new FileInputStream("src/P1072/input.txt"));
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        long games = Integer.parseInt(st.nextToken());
-        long wins = Integer.parseInt(st.nextToken());
+		long X = Integer.parseInt(st.nextToken()); // 게임 횟수
+		long Y = Integer.parseInt(st.nextToken()); // 이긴 횟수
+		long Z = Y * 100 / X; // 승률
 
-        // 현재 승률 계산
-        long currentWinRate = wins*100 / games;
+		long answer = -1;
+		long low = 0, high = Integer.MAX_VALUE;
 
-        // 이분 탐색 초기화
-        long left = 0;
-        long right = Integer.MAX_VALUE;
-        long answer = -1;
+		while (low <= high) {
+			long mid = (low + high) / 2;
 
-        // 이분 탐색
-        while(left <= right) {
-            long mid = (left + right)/2;
+			// 앞으로는 무조건 이기기만 함
+			long newX = X + mid;
+			long newY = Y + mid;
+			long newZ = newY * 100 / newX;
 
-            // 앞으로의 모든 게임에서 이기기만 함
-            long newWins = wins+mid;
-            long newGames = games+mid;
-            long newWinRate = newWins*100 / newGames;
+			if (newZ > Z) {
+				answer = mid;
+				high = mid - 1;
+			} else {
+				low = mid + 1;
+			}
+		}
 
-            if(newWinRate > currentWinRate) {
-                answer = mid;
-                right = mid-1;
-            } else {
-                left = mid+1;
-            }
-        }
+		System.out.println(answer);
+	}
 
-        System.out.println(answer);
-    }
 }
