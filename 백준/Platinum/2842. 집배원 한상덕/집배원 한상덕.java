b7c86b59535e262e4ac8676715b68cf6632ba496
jieunlee99 <105425346+jieunlee99@users.noreply.github.com>
2025-09-02 13:52:53 +0900

    [Platinum IV] Title: 집배원 한상덕, Time: 608 ms, Memory: 181052 KB -BaekjoonHub

--- 백준/Platinum/2842. 집배원 한상덕/집배원 한상덕.java
+++ 백준/Platinum/2842. 집배원 한상덕/집배원 한상덕.java
@@ -1,107 +1,121 @@
-import java.awt.Point;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    static int N, House, Min = Integer.MAX_VALUE;
-    static Point start;
-    static boolean[][] visit;
-    static char[][] map;
-    static int[][] arr;
-    static ArrayList<Integer> arrList = new ArrayList<>();
-    static int[] moveX = {0, 1, 1, 1, 0, -1, -1, -1};
-    static int[] moveY = {-1, -1, 0, 1, 1, 1, 0, -1};
 
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(br.readLine());
-        map = new char[N][N];
-        arr = new int[N][N];
+	static int N;
+	static int houseCount;
+	static int minFatigue = Integer.MAX_VALUE;
+	static int startX, startY;
 
-        for (int i = 0; i < N; i++) {
-            String str = br.readLine();
-            for (int j = 0; j < N; j++) {
-                char ch = str.charAt(j);
-                if (ch == 'P')
-                    start = new Point(j, i);
-                if (ch == 'K')
-                    House++;
+	static char[][] map;
+	static int[][] heights;
 
-                map[i][j] = ch;
-            }
-        }
-        for (int i = 0; i < N; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < N; j++) {
-                int val = Integer.parseInt(st.nextToken());
-                arr[i][j] = val;
-                if (!arrList.contains(val))
-                    arrList.add(val);
-            }
-        }
-        Collections.sort(arrList);
+	static ArrayList<Integer> uniqueHeights = new ArrayList<>();
 
-        bfs();
+	// 수직, 수평, 대각선 이동 가능
+	static int[] moveX = { 1, 1, 1, 0, 0, -1, -1, -1 };
+	static int[] moveY = { 1, 0, -1, 1, -1, 1, 0, -1 };
 
-    }
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
 
-    public static void bfs() {
-        int low = 0, high = 0;
-        while (low < arrList.size()) {
-            visit = new boolean[N][N];
-            Queue<Point> queue = new LinkedList<>();
-            int val = arr[start.y][start.x];
-            if (arrList.get(low) <= val && val <= arrList.get(high)) {
-                visit[start.y][start.x] = true;
-                queue.add(new Point(start.x, start.y));
-            }
-            
-            int count = 0;
-            while (!queue.isEmpty()) {
-                Point current = queue.poll();
+		N = Integer.parseInt(br.readLine());
 
-                // 방문한 집 개수 세기
-                if (map[current.y][current.x] == 'K') {
-                    count++;
-                }
+		map = new char[N][N];
+		heights = new int[N][N];
 
-                // 연결된 곳 순회
-                for (int d = 0; d < 8; d++) {
-                    int newY = current.y + moveY[d];
-                    int newX = current.x + moveX[d];
+		for (int i = 0; i < N; i++) {
+			String input = br.readLine();
 
-                    // 범위 내에 있고 방문한 적이 없다면 go
-                    if (0 <= newY && newY < N && 0 <= newX && newX < N && !visit[newY][newX]) {
-                        int nextVal = arr[newY][newX];
-                        // 다음 좌표의 고도가 범위 내에 있다면 go
-                        if (arrList.get(low) <= nextVal && nextVal <= arrList.get(high)) {
-                            visit[newY][newX] = true;
-                            queue.add(new Point(newX, newY));
-                        }
+			for (int j = 0; j < N; j++) {
+				map[i][j] = input.charAt(j);
 
-                    }
-                }
-            }
+				if (map[i][j] == 'P') {
+					startX = j;
+					startY = i;
+				} else if (map[i][j] == 'K') {
+					houseCount++;
+				}
+			}
+		}
 
-            // 모든 집에 방문할 수 있다면 최소값 업데이트
-            if (House == count) {
-                Min = Math.min(Min, arrList.get(high) - arrList.get(low));
-                // 더 작은 범위에서 모든 집에 방문할 수 있는지 확인하기 위해 고도 범위의 하한선 증가시킨다.
-                low++;
-            } else if (high + 1 < arrList.size()) {
-                // 모든 집을 방문할 수 없다면 고도 범위의 상한선을 증가시킨다.
-                high++;
-            } else {
-                // 가능한 범위를 모두 탐색했을 때는 종료시킨다.
-                break;
-            }
-        }
+		Set<Integer> set = new HashSet<>();
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < N; j++) {
+				heights[i][j] = Integer.parseInt(st.nextToken());
+				set.add(heights[i][j]);
+			}
+		}
+		uniqueHeights.addAll(set);
+		Collections.sort(uniqueHeights);
 
-        System.out.println(Min);
-    }
+		solve();
+	}
+
+	static void solve() {
+		int low = 0, high = 0;
+
+		while (low < uniqueHeights.size() && high < uniqueHeights.size()) {
+			if (canReachAllHouses(uniqueHeights.get(low), uniqueHeights.get(high))) {
+				minFatigue = Math.min(minFatigue, uniqueHeights.get(high) - uniqueHeights.get(low));
+				low++;
+			} else {
+				high++;
+			}
+		}
+
+		System.out.println(minFatigue);
+	}
+
+	static boolean canReachAllHouses(int minH, int maxH) {
+		int startHeight = heights[startY][startX];
+
+		if (startHeight < minH || startHeight > maxH) {
+			return false;
+		}
+
+		boolean[][] visited = new boolean[N][N];
+
+		Queue<int[]> queue = new LinkedList<>();
+
+		queue.add(new int[] { startY, startX });
+		visited[startY][startX] = true;
+
+		int count = 0;
+
+		while (!queue.isEmpty()) {
+			int[] current = queue.poll();
+			int y = current[0];
+			int x = current[1];
+
+			if (map[y][x] == 'K') {
+				count++;
+			}
+
+			for (int d = 0; d < 8; d++) {
+				int ny = y + moveY[d];
+				int nx = x + moveX[d];
+
+				if (nx < 0 || nx >= N || ny < 0 || ny >= N) {
+					continue;
+				}
+
+				if (visited[ny][nx]) {
+					continue;
+				}
+
+				int height = heights[ny][nx];
+				if (minH <= height && height <= maxH) {
+					visited[ny][nx] = true;
+					queue.add(new int[] { ny, nx });
+				}
+			}
+		}
+
+		return count == houseCount;
+	}
 }
