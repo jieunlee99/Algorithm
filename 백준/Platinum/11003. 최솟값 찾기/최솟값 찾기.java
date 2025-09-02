bf9905108e707a634b83df6c23fb3305a809c0cc
jieunlee99 <105425346+jieunlee99@users.noreply.github.com>
2025-09-02 14:15:38 +0900

    [Platinum V] Title: 최솟값 찾기, Time: 2260 ms, Memory: 728224 KB -BaekjoonHub

--- 백준/Platinum/11003. 최솟값 찾기/최솟값 찾기.java
+++ 백준/Platinum/11003. 최솟값 찾기/최솟값 찾기.java
@@ -1,50 +1,49 @@
-
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.*;
 
 public class Main {
 
-    static int N, L;
-    static int[] A;
+	public static void main(String[] args) throws IOException {
 
-    public static void main(String[] args) throws Exception {
-        // System.setIn(new FileInputStream("src/P11003/input.txt"));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int L = Integer.parseInt(st.nextToken());
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        L = Integer.parseInt(st.nextToken());
+		Deque<Node> deque = new LinkedList<>();
 
-        A = new int[N];
-        st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < N; i++) {
-            A[i] = Integer.parseInt(st.nextToken());
-        }
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			int num = Integer.parseInt(st.nextToken());
 
-        Deque<Integer> deque = new LinkedList<>();
+			//  맨 뒤와 비교하여 그 값보다 작으면 원래꺼를 삭제함
+			while (!deque.isEmpty() && deque.peekLast().num > num) {
+				deque.pollLast();
+			}
 
-        for (int i = 0; i < N; i++) {
-            // 1. 덱의 앞에서부터 슬라이딩 윈도우 밖에 있는 인덱스는 제거시킨다.
-            if (!deque.isEmpty() && deque.peekFirst() < i - L + 1) {
-                deque.pollFirst();
-            }
+			// 현재 값 추가
+			deque.add(new Node(i, num));
 
-            // 2. 덱의 뒤에서부터 현재 값보다 큰 값의 인덱스 제거 (최소값 유지)
-            while (!deque.isEmpty() && A[deque.peekLast()] > A[i]) {
-                deque.pollLast();
-            }
+			// 슬라이딩 윈도우 크기보다 오래 살아있었으면 삭제함
+			while (!deque.isEmpty() && deque.peekFirst().index < i - L + 1) {
+				deque.pollFirst();
+			}
 
-            // 3. 현재 인덱스 추가
-            deque.addLast(i);
+			// 현재의 최소값 출력 (저장)
+			sb.append(deque.peekFirst().num).append(" ");
+		}
 
-            // 4. 현재 슬라이딩 윈도우에서의 최소값을 결과에 추가
-            sb.append(A[deque.peekFirst()]).append(" ");
-        }
+		System.out.println(sb);
+	}
 
-        System.out.println(sb);
-    }
+	static class Node {
+		int index, num;
+
+		public Node(int index, int num) {
+			this.index = index;
+			this.num = num;
+		}
+	}
 }
