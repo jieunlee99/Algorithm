bd3e24c959c5e9551f2eb2dd2bab0848a3b4a56c
jieunlee99 <105425346+jieunlee99@users.noreply.github.com>
2025-09-04 14:42:05 +0900

    [Silver I] Title: 트리 순회, Time: 100 ms, Memory: 14164 KB -BaekjoonHub

--- 백준/Silver/1991. 트리 순회/트리 순회.java
+++ 백준/Silver/1991. 트리 순회/트리 순회.java
@@ -1,89 +1,92 @@
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
 
-    static int N;
-    static StringBuilder sb = new StringBuilder();
+	static StringBuilder sb = new StringBuilder();
 
-    public static void main(String[] args) throws Exception {
-        // System.setIn(new FileInputStream("src/P1991/input.txt"));
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        N = Integer.parseInt(br.readLine());
+		int N = Integer.parseInt(br.readLine());
 
-        Node head = new Node('A', null, null);
+		Node root = new Node('A', null, null);
 
-        StringTokenizer st;
-        for(int i=0; i<N; i++) {
-            st = new StringTokenizer(br.readLine());
-            char value = st.nextToken().charAt(0);
-            char left = st.nextToken().charAt(0);
-            char right = st.nextToken().charAt(0);
+		while (N-- > 0) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			char value = st.nextToken().charAt(0);
+			char left = st.nextToken().charAt(0);
+			char right = st.nextToken().charAt(0);
+			insert(root, value, left, right);
+		}
 
-            insert(head, value, left, right);
-        }
+		preOrder(root);
+		sb.append("\n");
+		inOrder(root);
+		sb.append("\n");
+		postOrder(root);
+		System.out.println(sb);
+	}
 
-        preOrder(head);
-        sb.append("\n");
-        inOrder(head);
-        sb.append("\n");
-        postOrder(head);
-        System.out.println(sb);
-    }
+	static void insert(Node node, char value, char left, char right) {
 
-    static void insert(Node node, char value, char left, char right) {
-        if(node.value == value) {
-            node.left = (left == '.' ? null : new Node(left, null, null));
-            node.right = (right == '.' ? null : new Node(right, null, null));
-        } else {
-            if(node.left != null) {
-                insert(node.left, value, left, right);
-            }
-            if(node.right != null) {
-                insert(node.right, value, left, right);
-            }
-        }
-    }
+		// 바로 삽입할 위치라면
+		if (node.value == value) {
+			node.left = (left == '.' ? null : new Node(left, null, null));
+			node.right = (right == '.' ? null : new Node(right, null, null));
+		}
 
-    static void preOrder(Node node) {
-        if(node == null) {
-            return;
-        }
-        sb.append(node.value);
-        preOrder(node.left);
-        preOrder(node.right);
-    }
+		// 삽입할 위치를 찾으러 내려감
+		else {
 
-    static void inOrder(Node node) {
-        if(node == null) {
-            return;
-        }
-        inOrder(node.left);
-        sb.append(node.value);
-        inOrder(node.right);
-    }
+			if (node.left != null) {
+				insert(node.left, value, left, right);
+			}
 
-    static void postOrder(Node node) {
-        if(node == null) {
-            return;
-        }
-        postOrder(node.left);
-        postOrder(node.right);
-        sb.append(node.value);
-    }
-}
+			if (node.right != null) {
+				insert(node.right, value, left, right);
+			}
+		}
+	}
 
-class Node {
-    char value;
-    Node left;
-    Node right;
+    // 전위
+	static void preOrder(Node node) {
+		if (node == null) {
+			return;
+		}
+		sb.append(node.value);
+		preOrder(node.left);
+		preOrder(node.right);
+	}
 
-    public Node(char value, Node left, Node right) {
-        this.value = value;
-        this.left = left;
-        this.right = right;
-    }
+    // 중위
+	static void inOrder(Node node) {
+		if (node == null) {
+			return;
+		}
+		inOrder(node.left);
+		sb.append(node.value);
+		inOrder(node.right);
+	}
+
+    // 후위
+	static void postOrder(Node node) {
+		if (node == null) {
+			return;
+		}
+		postOrder(node.left);
+		postOrder(node.right);
+		sb.append(node.value);
+	}
+
+	static class Node {
+		char value;
+		Node left, right;
+
+		public Node(char value, Node left, Node right) {
+			this.value = value;
+			this.left = left;
+			this.right = right;
+		}
+	}
 }
