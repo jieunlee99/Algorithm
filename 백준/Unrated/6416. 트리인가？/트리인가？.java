48e11e9e494eab07155fbb251cc3dbcb710527c4
jieunlee99 <105425346+jieunlee99@users.noreply.github.com>
2025-09-05 17:20:40 +0900

    [Unrated] Title: 트리인가?, Time: 188 ms, Memory: 17980 KB -BaekjoonHub

--- 백준/Unrated/6416. 트리인가？/트리인가？.java
+++ 백준/Unrated/6416. 트리인가？/트리인가？.java
@@ -1,51 +1,72 @@
-
 import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        StringTokenizer st;
 
-        Map<Integer, Integer> map;
-        Set<Integer> vertex;
+	public static void main(String[] args) throws IOException {
+		Scanner scanner = new Scanner(System.in);
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        out :
-        for(int i=1;;i++) {
-            map = new HashMap<>();
-            vertex = new HashSet<>();
-            boolean flag = false;
+		// <접점, 진입 간선의 개수>를 담는
+		Map<Integer, Integer> map;
 
-            st = new StringTokenizer(br.readLine());
-            while(true) {
-                if(!st.hasMoreTokens()) {
-                    st = new StringTokenizer(br.readLine());
-                }
-                int a = Integer.parseInt(st.nextToken());
-                int b = Integer.parseInt(st.nextToken());
-                if(a == 0) break;
-                if(a== -1) break out;
+		int t = 1; // t번째 테스트 케이스
 
-                if(!vertex.add(b)) { // 간선 2개이상 fail
-                    flag = true;
-                }
-                map.put(a, map.getOrDefault(a, 0)+1);
-            }
+		while (true) {
 
-            if(!vertex.isEmpty()) {
-                int rootNum=0;
-                for(int num : map.keySet()) {
-                    if(!vertex.contains(num)) rootNum++;
-                }
-                if(rootNum!=1) flag = true;
-            }
-            if(flag) {
-                sb.append("Case ").append(i).append(" is not a tree.\n");
-            }else {
-                sb.append("Case ").append(i).append(" is a tree.\n");
-            }
-        }
-        System.out.println(sb);
-    }
+			map = new HashMap<>();
+
+			int edge = 0; // 간선의 개수
+
+			while (true) {
+				int u = scanner.nextInt();
+				int v = scanner.nextInt();
+
+				// 하나의 테스트 케이스 종료
+				if (u == 0 && v == 0) {
+					break;
+				}
+
+				// 프로그램 종료
+				if (u == -1 && v == -1) {
+					return;
+				}
+
+				// u: 출발, v: 도착
+				map.put(u, map.getOrDefault(u, 0));
+				map.put(v, map.getOrDefault(v, 0) + 1);
+
+				edge++;
+			}
+
+			int root = 0; // 루트의 개수 (1개여야 함)
+
+			boolean isTree = true;
+
+			Iterator<Integer> key = map.keySet().iterator();
+			while (key.hasNext()) {
+				int num = key.next();
+
+				if (map.get(num) == 0) {
+					root++;
+				} else if (map.get(num) > 1) {
+					isTree = false;
+					break;
+				}
+			}
+
+			if (map.size() == 0 || (root == 1 && isTree && map.size() - 1 == edge)) {
+				bw.write("Case " + t + " is a tree.\n");
+			} else {
+				bw.write("Case " + t + " is not a tree.\n");
+			}
+
+			t++;
+
+			bw.flush();
+
+		}
+
+	}
+
 }
