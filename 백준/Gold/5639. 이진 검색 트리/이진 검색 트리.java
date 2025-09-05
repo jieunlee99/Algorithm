ac4b2d624678654455bf33eb7bee0246991c17ad
jieunlee99 <105425346+jieunlee99@users.noreply.github.com>
2025-09-05 18:01:09 +0900

    [Gold IV] Title: 이진 검색 트리, Time: 480 ms, Memory: 18080 KB -BaekjoonHub

--- 백준/Gold/5639. 이진 검색 트리/이진 검색 트리.java
+++ 백준/Gold/5639. 이진 검색 트리/이진 검색 트리.java
@@ -1,68 +1,60 @@
-
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.util.*;
 
 public class Main {
 
-    static StringBuilder sb = new StringBuilder();
+	static StringBuilder sb = new StringBuilder();
 
-    public static void main(String[] args) throws Exception {
-        // System.setIn(new FileInputStream("src/P5639/input.txt"));
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        Node root = new Node(Integer.parseInt(br.readLine()));
+		Node root = new Node(Integer.parseInt(br.readLine()));
 
-        String input;
-        while(true) {
-            input = br.readLine();
-            if(input == null || input.isEmpty()) {
-                break;
-            }
-            root.insert(Integer.parseInt(input));
-        }
+		while (true) {
+			String input = br.readLine();
+			if (input == null || input.isEmpty()) {
+				break;
+			}
+			root.insert(Integer.parseInt(input));
+		}
 
-        post(root);
+		postOrder(root);
 
-        System.out.print(sb);
-    }
-    static void post(Node node) {
-        if(node == null) {
-            return;
-        }
-        post(node.left);
-        post(node.right);
-        sb.append(node.value).append("\n");
-    }
-}
+		System.out.print(sb);
+	}
 
-class Node {
-    int value;
-    Node left, right;
+	static void postOrder(Node current) {
+		if (current == null) {
+			return;
+		}
+		postOrder(current.left);
+		postOrder(current.right);
+		sb.append(current.value).append("\n");
+	}
 
-    public Node(int value) {
-        this.value = value;
-    }
+	static class Node {
+		int value;
+		Node left, right;
 
-    public Node(int value, Node left, Node right) {
-        this.value = value;
-        this.left = left;
-        this.right = right;
-    }
+		public Node(int value) {
+			this.value = value;
+		}
 
-    void insert(int n) {
-        if(n < this.value) {
-            if(this.left == null) {
-                this.left = new Node(n);
-            } else {
-                this.left.insert(n);
-            }
-        } else {
-            if(this.right == null) {
-                this.right = new Node(n);
-            } else {
-                this.right.insert(n);
-            }
-        }
-    }
+		void insert(int value) {
+			if (value < this.value) {
+				if (this.left == null) {
+					this.left = new Node(value);
+				} else {
+					this.left.insert(value);
+				}
+			} else { // value >= this.value
+				if (this.right == null) {
+					this.right = new Node(value);
+				} else {
+					this.right.insert(value);
+				}
+			}
+		}
+	}
+
 }
