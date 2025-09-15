40f61c1c6bff5534c69808f0ef15e1b7142a237f
jieunlee99 <105425346+jieunlee99@users.noreply.github.com>
2025-09-15 14:50:48 +0900

    [Gold V] Title: 개똥벌레, Time: 280 ms, Memory: 31044 KB -BaekjoonHub

--- 백준/Gold/3020. 개똥벌레/개똥벌레.java
+++ 백준/Gold/3020. 개똥벌레/개똥벌레.java
@@ -1,55 +1,55 @@
-
 import java.io.BufferedReader;
-import java.io.FileInputStream;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
 
-    static int N, H;
+	static int N, H;
+	static int[] low, high;
 
-    public static void main(String[] args) throws Exception {
-        // System.setIn(new FileInputStream("src/P3020/input.txt"));
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken()); // 장애물의 개수
-        H = Integer.parseInt(st.nextToken()); // 장애물의 최대 높이
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken()); // 동굴의 길이
+		H = Integer.parseInt(st.nextToken()); // 동굴의 높이
 
-        // 종유석과 석순
-        int[] top = new int[H + 1];
-        int[] bottom = new int[H + 1];
+		low = new int[H + 1];
+		high = new int[H + 1];
 
-        // 장애물의 시작과 끝만 기록
-        for (int i = 0; i < N; i++) {
-            int height = Integer.parseInt(br.readLine());
-            if (i % 2 == 0) {
-                bottom[height]++;
-            } else {
-                top[height]++;
-            }
-        }
+		// 높이 입력
+		for (int i = 0; i < N; i++) {
+			int height = Integer.parseInt(br.readLine());
+			// 홀수번쨰에는 석순(아래), 짝수번쨰에는 종유석(위)
+			if (i % 2 == 0) {
+				low[height]++;
+			} else {
+				high[height]++;
+			}
+		}
 
-        // 누적합 계산
-        for (int i = H - 1; i >= 1; i--) {
-            bottom[i] += bottom[i + 1];
-            top[i] += top[i + 1];
-        }
+		// 누적합 계
+		for (int i = H - 1; i >= 1; i--) {
+			low[i] += low[i + 1];
+			high[i] += high[i + 1];
+		}
 
-        int minError = Integer.MAX_VALUE;
-        int cnt = 0;
+		int minError = Integer.MAX_VALUE; // 개똥벌레가 파괴해야 할 장애물의 최소 개수
+		int cnt = 0; // 장애물을 최소(minError)로만 만나는 경우의 수
 
-        // 높이마다 장애물 개수 계산 후 최소값 갱신
-        for (int i = 1; i <= H; i++) {
-            int totalErrors = bottom[i] + top[H - i + 1];
-            if (totalErrors < minError) {
-                minError = totalErrors;
-                cnt = 1;
-            } else if (totalErrors == minError) {
-                cnt++;
-            }
-        }
+		for (int i = 1; i <= H; i++) {
+			int totalErrors = low[i] + high[H - i + 1];
+			if (minError > totalErrors) {
+				minError = totalErrors;
+				cnt = 1;
+			} else if (minError == totalErrors) {
+				cnt++;
+			}
+		}
 
-        System.out.println(minError + " " + cnt);
-    }
+		System.out.println(minError + " " + cnt);
+	}
+
 }
