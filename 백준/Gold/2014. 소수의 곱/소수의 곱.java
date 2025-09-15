7e972feb3b2b4418de1dd888c66176976a373eee
jieunlee99 <105425346+jieunlee99@users.noreply.github.com>
2025-09-15 15:38:39 +0900

    [Gold I] Title: 소수의 곱, Time: 264 ms, Memory: 35880 KB -BaekjoonHub

--- 백준/Gold/2014. 소수의 곱/소수의 곱.java
+++ 백준/Gold/2014. 소수의 곱/소수의 곱.java
@@ -1,43 +1,49 @@
-
 import java.io.BufferedReader;
-import java.io.FileInputStream;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Main {
 
-    public static void main(String[] args) throws Exception {
-        // System.setIn(new FileInputStream("src/P2014/input.txt"));
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static int K, N;
+	static long[] primes;
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int K = Integer.parseInt(st.nextToken());
-        int N = Integer.parseInt(st.nextToken());
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        long[] primes = new long[K];
-        PriorityQueue<Long> pq = new PriorityQueue<>();
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		K = Integer.parseInt(st.nextToken());
+		N = Integer.parseInt(st.nextToken());
 
-        st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < K; i++) {
-            primes[i] = Long.parseLong(st.nextToken());
-            pq.add(primes[i]);
-        }
+		PriorityQueue<Long> pq = new PriorityQueue<>();
+		primes = new long[K];
 
-        long top = -1;
-        while (--N >= 0) {
-            top = pq.poll();
-            for (long prime : primes) {
-                // 범위를 벗어난다면 pass
-                if (top * prime >= (long) 2 << 31) break;
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < K; i++) {
+			primes[i] = Long.parseLong(st.nextToken());
+			pq.add(primes[i]);
+		}
 
-                pq.add(top * prime);
+		long top = -1; // top을 업데이트하면서 N번째 수를 찾는다.
+		while (N-- > 0) {
+			top = pq.poll();
+			for (long prime : primes) {
+				// 범위를 넘어가면 pass
+				if (top * prime >= (long) 2 << 31) {
+					break;
+				}
 
-                // top이 어떤 소수 prime으로 나누어 떨어진다는 것은 이미 그 소수와 곱해서 나온 값이라는 의미
-                if (top % prime == 0) break;
-            }
-        }
+				pq.add(top * prime);
 
-        System.out.println(top);
-    }
+				// 중복 pass
+				if (top % prime == 0) {
+					break;
+				}
+			}
+		}
+
+		System.out.println(top);
+	}
+
 }
