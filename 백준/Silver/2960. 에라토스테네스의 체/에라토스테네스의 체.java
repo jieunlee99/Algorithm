3f86d6ed089a52d000bbb7ea80e9be79fd837b67
jieunlee99 <105425346+jieunlee99@users.noreply.github.com>
2025-09-19 14:46:49 +0900

    [Silver IV] Title: 에라토스테네스의 체, Time: 100 ms, Memory: 14200 KB -BaekjoonHub

--- 백준/Silver/2960. 에라토스테네스의 체/에라토스테네스의 체.java
+++ 백준/Silver/2960. 에라토스테네스의 체/에라토스테네스의 체.java
@@ -1,44 +1,49 @@
-
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
 
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int K = Integer.parseInt(st.nextToken());
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int K = Integer.parseInt(st.nextToken());
+		System.out.println(findAnswer(N, K));
 
-        System.out.println(findAnswer(N, K));
-    }
+	}
 
-    static int findAnswer(int n, int k) {
-        int[] arr = new int[n+1];
+	static int findAnswer(int n, int k) {
 
-        for(int i=2; i<=n; i++) {
-            arr[i] = i;
-        }
+		int[] arr = new int[n + 1];
 
-        for(int i=2; i<=n; i++) {
-            // 이미 지워진 값은 건너뛰기
-            if(arr[i] == 0) continue;
+		for (int i = 2; i <= n; i++) {
+			arr[i] = i;
+		}
 
-            for(int j=i; j<=n; j+=i) {
-                if(arr[j]!=0) {
-                    arr[j] = 0;
-                    k--;
+		for (int i = 2; i <= n; i++) {
+			// 이미 지웠던 숫자는 pass
+			if (arr[i] == 0)
+				continue;
 
-                    if(k==0) {
-                        return j;
-                    }
-                }
-            }
-        }
+			// 현재 숫자의 배수들을 제거함
+			for (int j = i; j <= n; j += i) {
+				if (arr[j] != 0) {
+					arr[j] = 0;
+					k--;
 
-        return -1;
-    }
+					// k번째 수를 찾았다면 Return
+					if (k == 0) {
+						return j;
+					}
+				}
+			}
+
+		}
+
+		return -1;
+	}
 }
