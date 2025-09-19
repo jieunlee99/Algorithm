e9ae12ebe512fa86b8f3149638496e117d203750
jieunlee99 <105425346+jieunlee99@users.noreply.github.com>
2025-09-19 16:28:45 +0900

    [Bronze III] Title: 암호제작, Time: 248 ms, Memory: 37756 KB -BaekjoonHub

--- 백준/Bronze/1837. 암호제작/암호제작.java
+++ 백준/Bronze/1837. 암호제작/암호제작.java
@@ -1,50 +1,47 @@
-
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.math.BigInteger;
+import java.util.*;
 
 public class Main {
 
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static boolean[] isPrime;
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        String P = st.nextToken();
-        int K = Integer.parseInt(st.nextToken());
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		String Pstr = st.nextToken();
+		int K = Integer.parseInt(st.nextToken());
 
-        boolean[] isPrime = new boolean[K];
-        for(int i=2; i<K; i++) {
-            isPrime[i] = true;
-        }
+		BigInteger P = new BigInteger(Pstr);
 
-        for(int i=2; i*i<K; i++) {
-            if(isPrime[i]) {
-                for(int j=i*i; j<K; j+=i) {
-                    isPrime[j] = false;
-                }
-            }
-        }
+		// 1. K까지 소수 구하기
+		isPrime = new boolean[K];
+		Arrays.fill(isPrime, true);
+		isPrime[0] = isPrime[1] = false;
+		for (int i = 2; i * i < K; i++) {
+			if (isPrime[i]) {
+				for (int j = i * i; j < K; j += i) {
+					isPrime[j] = false;
+				}
+			}
+		}
 
-        for(int i=2; i<K; i++) {
-            if(isPrime[i]) {
-                if(mod(P, i) == 0) {
-                    System.out.println("BAD "+i);
-                    return;
-                }
-            }
-        }
+		// 2. 소수로 나눗셈 확인 (K까지의 숫자 중에 나누어 떨어지는 수가 있으면 안 됨)
+		for (int i = 2; i < K; i++) {
+			if (isPrime[i]) {
+				if (mod(P, i)) {
+					System.out.println("BAD " + i);
+					return;
+				}
+			}
+		}
 
-        System.out.println("GOOD");
-    }
+		// 3. 나누어떨어지는 게 없으면 GOOD
+		System.out.println("GOOD");
+	}
 
-    static int mod(String num, int divisor) {
-        int result = 0;
-        for (int i = 0; i < num.length(); i++) {
-            result = (result * 10 + (num.charAt(i) - '0')) % divisor;
-        }
-        return result;
-    }
+	private static boolean mod(BigInteger P, int i) {
+		return P.mod(BigInteger.valueOf(i)).equals(BigInteger.ZERO);
+	}
 }
-
-
