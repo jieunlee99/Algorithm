52598b8438410b41fc000f6487bdc6c2fc60cc98
jieunlee99 <105425346+jieunlee99@users.noreply.github.com>
2025-09-23 19:03:25 +0900

    [Silver II] Title: 최대공약수, Time: 272 ms, Memory: 21224 KB -BaekjoonHub

--- 백준/Silver/2824. 최대공약수/최대공약수.java
+++ 백준/Silver/2824. 최대공약수/최대공약수.java
@@ -1,39 +1,37 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.math.BigInteger;
 import java.util.StringTokenizer;
 
 public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-    static int N, M;
+		BigInteger A = BigInteger.ONE;
+		BigInteger B = BigInteger.ONE;
 
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
+		int N = Integer.parseInt(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			BigInteger num = new BigInteger(st.nextToken());
+			A = A.multiply(num);
+		}
 
-        BigInteger numA = BigInteger.ONE;
-        BigInteger numB= BigInteger.ONE;
+		int M = Integer.parseInt(br.readLine());
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < M; i++) {
+			BigInteger num = new BigInteger(st.nextToken());
+			B = B.multiply(num);
+		}
 
-        N = Integer.parseInt(br.readLine());
-        st = new StringTokenizer(br.readLine());
-        for(int i=0; i<N; i++) {
-            BigInteger num = new BigInteger(st.nextToken());
-            numA = numA.multiply(num);
-        }
+		BigInteger gcd = A.gcd(B);
 
-        M = Integer.parseInt(br.readLine());
-        st = new StringTokenizer(br.readLine());
-        for(int i=0; i<M; i++) {
-            BigInteger num = new BigInteger(st.nextToken());
-            numB = numB.multiply(num);
-        }
+		String result = gcd.toString();
+		if (result.length() > 9) {
+			result = result.substring(result.length() - 9);// 마지막 9자리만 남김
+		}
+		System.out.println(result);
+	}
 
-        BigInteger gcd = numA.gcd(numB);
-
-        String result = gcd.toString();
-        if (result.length() > 9) {
-            result = result.substring(result.length() - 9);  // 마지막 9자리 추출
-        }
-        System.out.println(result);
-    }
 }
