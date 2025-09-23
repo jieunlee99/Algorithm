ce412149ad61150c406a76635b48018dd0944af0
jieunlee99 <105425346+jieunlee99@users.noreply.github.com>
2025-09-24 00:05:18 +0900

    [Silver III] Title: 1, Time: 96 ms, Memory: 14136 KB -BaekjoonHub

--- 백준/Silver/4375. 1/1.java
+++ 백준/Silver/4375. 1/1.java
@@ -1,31 +1,33 @@
-
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
 
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
 
-        String input;
-        while ((input = br.readLine()) != null) {
-            int n = Integer.parseInt(input);
+		String input;
+		while ((input = br.readLine()) != null) {
+			
+			int n = Integer.parseInt(input);
 
-            long num = 1;
-            int cnt = 1;
+			long num = 1;
+			long cnt = 1;
 
-            while (true) {
-                if (num % n == 0) {
-                    sb.append(cnt).append("\n");
-                    break;
-                }
+			while (true) {
+				if (num % n == 0) {
+					sb.append(cnt).append("\n");
+					break;
+				}
 
-                num = (num * 10 + 1) % n;
-                cnt++;
-            }
-        }
+				num = (num * 10 + 1) % n;
+				cnt++;
+			}
+		}
 
-        System.out.print(sb);
-    }
+		System.out.println(sb);
+	}
+
 }
