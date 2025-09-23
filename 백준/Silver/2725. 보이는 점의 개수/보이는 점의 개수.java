4513c2e511ce5c3edcfe7cac03de2c954ca22ccf
jieunlee99 <105425346+jieunlee99@users.noreply.github.com>
2025-09-23 23:32:05 +0900

    [Silver II] Title: 보이는 점의 개수, Time: 148 ms, Memory: 14424 KB -BaekjoonHub

--- 백준/Silver/2725. 보이는 점의 개수/보이는 점의 개수.java
+++ 백준/Silver/2725. 보이는 점의 개수/보이는 점의 개수.java
@@ -1,37 +1,41 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
 
-        int C = Integer.parseInt(br.readLine());
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
 
-        int[] cnt = new int[1001];
-        cnt[1] = 3;
+		int C = Integer.parseInt(br.readLine());
 
-        int tmp; // 서로소 쌍의 개수를 저장할 변수
-        for(int i=2; i<1001; i++) {
-            tmp = 0;
-            for(int j=1; j<i; j++) {
-                if(gcd(i, j) == 1) {
-                    tmp++;
-                }
-            }
-            cnt[i] = cnt[i-1]+tmp*2;
-        }
+		int[] cnt = new int[1001];
+		cnt[1] = 3;
 
-        while (C-- > 0) {
-            int n = Integer.parseInt(br.readLine());
-            sb.append(cnt[n]).append("\n");
-        }
+		for (int i = 2; i <= 1000; i++) {
+			int tmp = 0;
+			for (int j = 1; j < i; j++) {
+				if (gcd(i, j) == 1) {
+					tmp++;
+				}
+			}
+			cnt[i] = cnt[i - 1] + tmp * 2; // 대칭을 고려하여 tmp에 2를 곱해 더해준다.
+		}
 
-        System.out.print(sb);
-    }
+		while (C-- > 0) {
+			int N = Integer.parseInt(br.readLine());
+			sb.append(cnt[N]).append("\n");
+		}
 
-    static int gcd(int a, int b) {
-        if (b == 0) return a;
-        return gcd(b, a % b);
-    }
+		System.out.println(sb);
+	}
+
+	static int gcd(int a, int b) {
+		if (a % b == 0) {
+			return b;
+		}
+		return gcd(b, a % b);
+	}
+
 }
