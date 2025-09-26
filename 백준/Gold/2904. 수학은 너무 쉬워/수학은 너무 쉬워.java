de784b4542dc5f5377613143ba137f748e3004a8
jieunlee99 <105425346+jieunlee99@users.noreply.github.com>
2025-09-26 14:55:07 +0900

    [Gold III] Title: 수학은 너무 쉬워, Time: 144 ms, Memory: 19944 KB -BaekjoonHub

--- 백준/Gold/2904. 수학은 너무 쉬워/수학은 너무 쉬워.java
+++ 백준/Gold/2904. 수학은 너무 쉬워/수학은 너무 쉬워.java
@@ -1,67 +1,67 @@
 
 import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.io.IOException;
-import java.util.Arrays;
+import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    static final int MAX = 1000000 + 1;
 
-    static int[] arr = new int[100]; // 입력 저장
-    static int[] factor = new int[MAX]; // 빈도수 저장
+	static final int MAX = 1_000_000;
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static int[] arr = new int[100]; // 입력 수 저장
+	static int[] factor = new int[MAX + 1]; // 소인수별 지수 합 저장
 
-        int n = Integer.parseInt(br.readLine());
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < n; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
-            countFactor(arr[i]); // 빈도 수 계산
-        }
+		int N = Integer.parseInt(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int score = 1;
-        int move = 0;
+		for (int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+			countFactor(arr[i]); // 입력된 수를 소인수분해해서 factor 누적
+		}
 
-        for (int i = 2; i < MAX; i++) {
-            if (factor[i] >= n) {
-                int repeat = factor[i] / n;
-                int want = 1;
-                
-                for (int j = 0; j < repeat; j++) {
-                    want *= i;
-                }
+		int score = 1; // 문제에서 구하는 특별한 GCD
+		int move = 0; // 연산 횟수
 
-                for (int j = 0; j < n; j++) {
-                    int temp = arr[j];
-                    while (temp % want != 0) { // want로 나눠지지 않는다면 추가
-                        temp *= i;
-                        move++; // 움직임 추가
-                    }
-                }
+		// 소인수별 평균 지수 기반으로 GCD 만들기
+		for (int i = 2; i <= MAX; i++) {
+			int repeat = factor[i] / N; // 평균 지수
+			
+			if (repeat > 0) {
+				// GCD에 i^repeat 포함
+				for (int j = 0; j < repeat; j++) {
+					score *= i;
+				}
 
-                // 최대공약수에 곱해줌
-                while (repeat-- > 0) {
-                    score *= i;
-                }
-            }
-        }
+				// move 계산: 각 수가 가진 i의 지수와 repeat 비교
+				for (int j = 0; j < N; j++) {
+					int cnt = 0, tmp = arr[j];
+					while (tmp % i == 0) {
+						cnt++;
+						tmp /= i;
+					}
+					if (cnt < repeat) {
+						move += (repeat - cnt);
+					}
+				}
+			}
+		}
 
-        System.out.println(score + " " + move);
-    }
+		System.out.println(score + " " + move);
+	}
 
-    public static void countFactor(int n) {
-        for (int i = 2; i * i <= n; i++) {
-            while (n % i == 0) {
-                n /= i;
-                factor[i]++;
-            }
-        }
-        if (n != 1) {                 // n이 1이 아니면 남은 소인수로 취급
-            factor[n]++;
-        }
-    }
+	// 소인수분해: 각 소수의 지수를 factor 배열에 누적
+	static void countFactor(int n) {
+		for (int i = 2; i * i <= n; i++) {
+			while (n % i == 0) {
+				n /= i;
+				factor[i]++;
+			}
+		}
+		if (n > 1) {
+			factor[n]++;
+		}
+	}
 }
-
