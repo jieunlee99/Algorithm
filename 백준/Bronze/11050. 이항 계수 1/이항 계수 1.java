87cd403661e5e021e7a08618cf239e088ec7c5bc
jieunlee99 <105425346+jieunlee99@users.noreply.github.com>
2025-09-26 16:37:39 +0900

    [Bronze I] Title: 이항 계수 1, Time: 104 ms, Memory: 14256 KB -BaekjoonHub

--- 백준/Bronze/11050. 이항 계수 1/이항 계수 1.java
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.java
@@ -1,22 +1,26 @@
+
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int K = Integer.parseInt(st.nextToken());
+	public static void main(String[] args) throws IOException {
 
-        System.out.println(factorial(N)/(factorial(N-K)*factorial(K)));
-    }
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-    static int factorial(int n) {
-        int result = 1;
-        for(int i=1; i<=n; i++) {
-            result *= i;
-        }
-        return result;
-    }
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int K = Integer.parseInt(st.nextToken());
+
+		System.out.println(factorial(N)/(factorial(N-K)*factorial(K)));
+	}
+
+	static int factorial(int n) {
+		int result = 1;
+		for (int i = 1; i <= n; i++) {
+			result *= i;
+		}
+		return result;
+	}
 }
