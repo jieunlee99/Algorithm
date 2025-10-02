16961ffc230c5f4623dc83c4758ab72159ecf12e
jieunlee99 <105425346+jieunlee99@users.noreply.github.com>
2025-10-02 16:10:31 +0900

    [Silver II] Title: N과 M (10), Time: 104 ms, Memory: 14272 KB -BaekjoonHub

--- 백준/Silver/15664. N과 M （10）/N과 M （10）.java
+++ 백준/Silver/15664. N과 M （10）/N과 M （10）.java
@@ -1,35 +1,46 @@
-import java.util.*;
-import java.io.*;
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.StringTokenizer;
 
 public class Main {
 
 	static int N, M;
-	static int[] nums;
-	static ArrayList<Integer> selected;
+	static int[] num;
+	static boolean[] visited;
+	static List<Integer> selected;
 	static StringBuilder sb = new StringBuilder();
 
-	public static void main(String[] args) throws Exception {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken()); // N개의 자연수를 준다.
-		M = Integer.parseInt(st.nextToken()); // M개를 고른다.
 
-		nums = new int[N];
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+
+		num = new int[N];
+		visited = new boolean[N];
 		selected = new ArrayList<>();
 
 		st = new StringTokenizer(br.readLine());
 		for (int i = 0; i < N; i++) {
-			nums[i] = Integer.parseInt(st.nextToken());
+			num[i] = Integer.parseInt(st.nextToken());
 		}
-		Arrays.sort(nums);
+		Arrays.sort(num);
 
 		backtracking(0, 0);
 
-		System.out.println(sb.toString());
+		System.out.println(sb);
+
 	}
 
 	static void backtracking(int depth, int start) {
+
 		if (depth == M) {
 			for (int num : selected) {
 				sb.append(num).append(" ");
@@ -38,16 +49,22 @@ public class Main {
 			return;
 		}
 
-		int lastNum = -1;
+		int lastVisitNum = -1;
+
 		for (int i = start; i < N; i++) {
-			if (nums[i] != lastNum) { // 갈 수 있다면?
-				selected.add(nums[i]);
-				backtracking(depth + 1, i + 1); // 간다.
-				selected.remove(selected.size() - 1);
-				lastNum = nums[i];
+			if (num[i] != lastVisitNum) {
+
+				visited[i] = true; // 체크인
+				selected.add(num[i]);
+
+				backtracking(depth + 1, i + 1); // Go
+
+				selected.remove(selected.size() - 1); // 체크아웃
+				visited[i] = false;
+
+				lastVisitNum = num[i]; // 중복되는 값이 들어올 수 있기 때문에 저장함
 			}
 		}
-
 	}
 
 }
