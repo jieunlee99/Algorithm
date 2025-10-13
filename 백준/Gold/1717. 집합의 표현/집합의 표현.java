d28d4c1ca05608692940c97c4d891b8910424ff6
jieunlee99 <105425346+jieunlee99@users.noreply.github.com>
2025-10-13 15:49:14 +0900

    [Gold V] Title: 집합의 표현, Time: 428 ms, Memory: 53744 KB -BaekjoonHub

--- 백준/Gold/1717. 집합의 표현/집합의 표현.java
+++ 백준/Gold/1717. 집합의 표현/집합의 표현.java
@@ -1,54 +1,52 @@
-import java.util.*;
-import java.io.*;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
 
 	static int N, M;
 	static int[] parent;
 
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
 
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
+		N = Integer.parseInt(st.nextToken()); // 초기 n+1개의 집합: {0}, {1}, ..., {n}
+		M = Integer.parseInt(st.nextToken()); // 연산의 개수
 
-		// 부모 배열 초기화
 		parent = new int[N + 1];
 		for (int i = 0; i <= N; i++) {
 			parent[i] = i;
 		}
 
-		StringBuilder sb = new StringBuilder();
-		for (int m = 0; m < M; m++) {
+		while (M-- > 0) {
 			st = new StringTokenizer(br.readLine());
-			int op = Integer.parseInt(st.nextToken());
+			int type = Integer.parseInt(st.nextToken());
 			int a = Integer.parseInt(st.nextToken());
 			int b = Integer.parseInt(st.nextToken());
 
-			if (op == 0) {
+			// 합집합
+			if (type == 0) {
 				union(a, b);
-			} else if (op == 1) {
+			}
+
+			// a와 b가 같은 집합에 속하는지 확인 - YES/NO
+			else if (type == 1) {
 				if (find(a) == find(b)) {
-					sb.append("YES\n");
+					sb.append("YES").append("\n");
 				} else {
-					sb.append("NO\n");
+					sb.append("NO").append("\n");
 				}
 			}
 		}
 
-		System.out.print(sb.toString());
+		System.out.println(sb);
 	}
 
-	// find 함수: 경로 압축 적용
-	static int find(int num) {
-		if (parent[num] != num) {
-			parent[num] = find(parent[num]); // 경로 압축
-		}
-		return parent[num];
-	}
-
-	// union 함수: 두 원소를 합치는 연산
+	// 합집합 수행
 	static void union(int a, int b) {
 		int aRoot = find(a);
 		int bRoot = find(b);
@@ -57,4 +55,13 @@ public class Main {
 			parent[aRoot] = bRoot;
 		}
 	}
+
+	// 해당 원소의 root를 찾아줌
+	static int find(int num) {
+		if (parent[num] != num) {
+			parent[num] = find(parent[num]); // 경로 압축
+		}
+		return parent[num];
+	}
+
 }
