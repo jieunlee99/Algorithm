0c6a2ee3fcb3f7f6400e2aa8cb6c6011eb1af450
jieunlee99 <105425346+jieunlee99@users.noreply.github.com>
2025-10-14 14:00:48 +0900

    [Gold IV] Title: 키 순서, Time: 516 ms, Memory: 36968 KB -BaekjoonHub

--- 백준/Gold/2458. 키 순서/키 순서.java
+++ 백준/Gold/2458. 키 순서/키 순서.java
@@ -1,63 +1,64 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
 
 	static final int INF = 100 * 100_000 + 1;
 	static int N, M;
 	static int[][] dist;
-	
-	public static void main(String[] args) throws Exception {
+
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		
+
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		N = Integer.parseInt(st.nextToken()); // 학생 수
-		M = Integer.parseInt(st.nextToken()); // 비교 횟수
-		
-		dist = new int[N+1][N+1];
-		for(int i=1; i<=N; i++) {
+		M = Integer.parseInt(st.nextToken()); // 키 비교 횟수
+
+		dist = new int[N + 1][N + 1];
+		for (int i = 1; i <= N; i++) {
 			Arrays.fill(dist[i], INF);
 			dist[i][i] = 0;
 		}
-	
-		for(int i=0; i<M; i++) {
+
+		while (M-- > 0) {
 			st = new StringTokenizer(br.readLine());
+			// a가 b보다 작다
 			int a = Integer.parseInt(st.nextToken());
 			int b = Integer.parseInt(st.nextToken());
 			dist[a][b] = Math.min(dist[a][b], 1);
 		}
-		
-		floydWarshall();
-		
-		int cnt = 0;
-		
-		for(int i=1; i<=N; i++) {
-			for(int j=1; j<=N; j++) {
-				if(dist[i][j] == INF && dist[j][i]== INF) {
+
+		fw();
+
+		// 자기 키가 몇번째인지 알 수 있는 사람의 수
+		int answer = 0;
+
+		for (int i = 1; i <= N; i++) {
+			for (int j = 1; j <= N; j++) {
+				// 자신과 비교하지 않은 사람이 있다면 no
+				if (dist[i][j] == INF && dist[j][i] == INF) {
 					break;
 				}
 				
-				if(j==N) cnt++;
+				if (j == N)
+					answer++;
 			}
 		}
 		
-		bw.write(String.valueOf(cnt));
-		
-		bw.flush();
-		
-		br.close();
-		bw.close();
+		System.out.println(answer);
 	}
 
-	static void floydWarshall() {
-		for(int k=1; k<=N; k++) {
-			for(int i=1; i<=N; i++) {
-				for(int j=1; j<=N; j++) {
-					if(dist[i][k] == INF || dist[k][j] == INF) {
+	static void fw() {
+		for (int k = 1; k <= N; k++) {
+			for (int i = 1; i <= N; i++) {
+				for (int j = 1; j <= N; j++) {
+					if (dist[i][k] == INF || dist[k][j] == INF) {
 						continue;
 					}
-					dist[i][j] = Math.min(dist[i][j], dist[i][k]+dist[k][j]); 
+					dist[i][j] = Math.min(dist[i][j], dist[i][k] + dist[k][j]);
 				}
 			}
 		}
