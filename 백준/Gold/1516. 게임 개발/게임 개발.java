8e1693fb2ab67ea5d5a2e7bd7700ea446976927e
jieunlee99 <105425346+jieunlee99@users.noreply.github.com>
2025-10-14 14:44:18 +0900

    [Gold III] Title: 게임 개발, Time: 224 ms, Memory: 21828 KB -BaekjoonHub

--- 백준/Gold/1516. 게임 개발/게임 개발.java
+++ 백준/Gold/1516. 게임 개발/게임 개발.java
@@ -1,78 +1,77 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class Main {
 
 	static int N;
-	
+
 	static int[] time;
 	static int[] indegree;
 	static int[] answer;
-	static List<Integer>[] graph;
+	static List<Integer>[] adjList;
 	static Queue<Integer> queue;
-	
-	static StringTokenizer st;
-	
-	public static void main(String[] args) throws Exception {
-		
+
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		
+
 		N = Integer.parseInt(br.readLine());
-		
-		time = new int[N+1];
-		indegree = new int[N+1];
-		answer = new int[N+1];
-		graph = new ArrayList[N+1];
-		for(int i=1; i<=N; i++) {
-			graph[i] = new ArrayList<>();
+
+		time = new int[N + 1];
+		indegree = new int[N + 1];
+		answer = new int[N + 1];
+		adjList = new ArrayList[N + 1];
+		for (int i = 1; i <= N; i++) {
+			adjList[i] = new ArrayList<>();
 		}
 		queue = new LinkedList<>();
-		
-		for(int i=1; i<=N; i++) {
-			st = new StringTokenizer(br.readLine());
+
+		for (int i = 1; i <= N; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
 			time[i] = Integer.parseInt(st.nextToken());
-			
-			while(st.hasMoreTokens()) {
+
+			while (true) {
 				int num = Integer.parseInt(st.nextToken());
-				
-				if(num == -1) {
+				if (num == -1) {
 					break;
 				}
-				
-				graph[num].add(i); // num -> i
+
+				adjList[num].add(i); // num 건물을 지은 후에 i 건물을 지어야 함
 				indegree[i]++;
 			}
 		}
-		
-		for(int i=1; i<=N; i++) {
-			if(indegree[i] == 0) {
+
+		// 진입차수가 0인 건물을 큐에 넣어줌
+		for (int i = 1; i <= N; i++) {
+			if (indegree[i] == 0) {
+				answer[i] = time[i]; // 무조건 자기 건물을 짓는 시간은 포함하게 됨
 				queue.offer(i);
-				answer[i] = time[i];
 			}
 		}
-		
-		while(!queue.isEmpty()) {
+
+		while (!queue.isEmpty()) {
 			int current = queue.poll();
-			
-			for(int next:graph[current]) {
+
+			for (int next : adjList[current]) {
 				indegree[next]--;
-				answer[next] = Math.max(answer[next], answer[current]+time[next]);
-				
-				if(indegree[next] == 0) {
+				answer[next] = Math.max(answer[next], answer[current] + time[next]);
+
+				if (indegree[next] == 0) {
 					queue.offer(next);
 				}
 			}
 		}
-		
+
+		StringBuilder sb = new StringBuilder();
 		for (int i = 1; i <= N; i++) {
-			bw.write(answer[i] + "\n");
+			sb.append(answer[i]).append("\n");
 		}
-		
-		bw.flush();
-		
-		br.close();
-		bw.close();
+		System.out.print(sb);
 	}
 
 }
