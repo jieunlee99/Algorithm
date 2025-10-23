14eb32fd29d16c493708a13c6e09b70ae1f5911e
jieunlee99 <105425346+jieunlee99@users.noreply.github.com>
2025-10-23 15:48:05 +0900

    [Gold IV] Title: 최단경로, Time: 688 ms, Memory: 109264 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.java
+++ 백준/Gold/1753. 최단경로/최단경로.java
@@ -1,16 +1,16 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
 
-	static int V, E; // 정점, 간선 개수
-	static int K; // start
+	static int V, E; // 정점, 간선의 개수
+	static int K; // 시작 정점
 
-	static ArrayList<Edge>[] adjList;
+	static List<Edge>[] adjList;
 	static boolean[] visited;
 	static int[] dist;
 
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
@@ -25,77 +25,68 @@ public class Main {
 		visited = new boolean[V + 1];
 		dist = new int[V + 1];
 
-		K = Integer.parseInt(br.readLine()); // 시작 정점
+		K = Integer.parseInt(br.readLine());
 
-		int u, v, w;
 		for (int i = 0; i < E; i++) {
 			st = new StringTokenizer(br.readLine());
-			u = Integer.parseInt(st.nextToken());
-			v = Integer.parseInt(st.nextToken());
-			w = Integer.parseInt(st.nextToken());
-			adjList[u].add(new Edge(v, w));
+			int u = Integer.parseInt(st.nextToken());
+			int v = Integer.parseInt(st.nextToken());
+			int w = Integer.parseInt(st.nextToken());
+			adjList[u].add(new Edge(v, w)); // 방향있는 간선
 		}
-		
+
 		dijkstra();
-		
-		for(int i=1; i<=V; i++) {
-			if(dist[i] == Integer.MAX_VALUE) {
+
+		for (int i = 1; i <= V; i++) {
+			if (dist[i] == Integer.MAX_VALUE) {
 				bw.write("INF\n");
 			} else {
-				bw.write(dist[i]+"\n");
+				bw.write(dist[i] + "\n");
 			}
 		}
+
 		bw.flush();
-		
 		bw.close();
 		br.close();
 	}
 
 	static void dijkstra() {
 		PriorityQueue<Edge> pq = new PriorityQueue<>();
-		
+
 		Arrays.fill(dist, Integer.MAX_VALUE);
 		dist[K] = 0;
 		pq.offer(new Edge(K, 0));
-		
-		while(!pq.isEmpty()) {
+
+		while (!pq.isEmpty()) {
 			Edge current = pq.poll();
-			
-			if(visited[current.to]) {
+
+			if (visited[current.to]) {
 				continue;
 			}
-			
+
 			visited[current.to] = true;
-			
-			for(Edge next : adjList[current.to]) {
-				if(dist[next.to] > next.weight + current.weight) {
-					dist[next.to] = next.weight + current.weight;
+
+			for (Edge next : adjList[current.to]) {
+				if (dist[next.to] > next.cost + current.cost) {
+					dist[next.to] = next.cost + current.cost;
 					pq.offer(new Edge(next.to, dist[next.to]));
 				}
 			}
 		}
 	}
-}
 
-class Edge implements Comparable<Edge> {
-	int to;
-	int weight;
+	static class Edge implements Comparable<Edge> {
+		int to, cost;
 
-	public Edge(int to, int weight) {
-		this.to = to;
-		this.weight = weight;
-	}
+		public Edge(int to, int cost) {
+			this.to = to;
+			this.cost = cost;
+		}
 
-	public int getTo() {
-		return to;
-	}
+		@Override
+		public int compareTo(Edge o) {
+			return this.cost - o.cost;
+		}
 
-	public int getWeight() {
-		return weight;
-	}
-
-	@Override
-	public int compareTo(Edge o) {
-		return weight - o.weight;
 	}
 }
