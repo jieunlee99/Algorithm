bd209fc68e29cddecc8b55d21148c4ac59bf61b8
jieunlee99 <105425346+jieunlee99@users.noreply.github.com>
2025-10-23 16:08:23 +0900

    [Gold V] Title: 최소비용 구하기, Time: 460 ms, Memory: 50660 KB -BaekjoonHub

--- 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.java
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.java
@@ -1,78 +1,91 @@
 
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
 
 public class Main {
+
+	static int N, M;
+	static int start, end;
+
+	static int[] dist;
+	static boolean[] visited;
+	static List<Edge>[] adjList;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
 
-		int N = Integer.parseInt(br.readLine());
-		int M = Integer.parseInt(br.readLine());
+		N = Integer.parseInt(br.readLine()); // 도시의 개수
+		M = Integer.parseInt(br.readLine()); // 버스의 개수
 
-		ArrayList<ArrayList<Node>> adjList = new ArrayList<>();
-		for (int i = 0; i <= N; i++) {
-			adjList.add(new ArrayList<>());
+		dist = new int[N + 1];
+		visited = new boolean[N + 1];
+		adjList = new ArrayList[N + 1];
+		for (int i = 1; i <= N; i++) {
+			adjList[i] = new ArrayList<>();
 		}
 
-		// from에서 to로 갈 때 cost만큼의 비용이 든다.
 		for (int i = 0; i < M; i++) {
 			st = new StringTokenizer(br.readLine());
-			int from = Integer.parseInt(st.nextToken());
-			int to = Integer.parseInt(st.nextToken());
-			int cost = Integer.parseInt(st.nextToken());
-			adjList.get(from).add(new Node(to, cost));
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			int c = Integer.parseInt(st.nextToken());
+			adjList[a].add(new Edge(b, c));
 		}
 
-		// 시작점, 도착점
 		st = new StringTokenizer(br.readLine());
-		int start = Integer.parseInt(st.nextToken());
-		int end = Integer.parseInt(st.nextToken());
+		start = Integer.parseInt(st.nextToken());
+		end = Integer.parseInt(st.nextToken());
 
-		// 다익스트라 알고리즘: 모든 노드를 무한대로 초기화
-		int[] dist = new int[N + 1];
-		for (int i = 0; i <= N; i++) {
-			dist[i] = Integer.MAX_VALUE;
-		}
+		dijkstra();
 
-		// 우선순위 큐 - 비용 내림차순 정렬
-		PriorityQueue<Node> pq = new PriorityQueue<Node>((o1, o2) -> Integer.compare(o1.cost, o2.cost));
-
-		// 시작점 초기화
-		pq.offer(new Node(start, 0));
-		dist[start] = 0;
-
-		while (!pq.isEmpty()) {
-			Node current = pq.poll();
-
-			// 현재 노드의 비용이 더 비싸다면 pass
-			if (dist[current.idx] < current.cost) {
-				continue;
-			}
-
-			// 현재 노드에서 갈 수 있는 모든 노드 탐색
-			for (int i = 0; i < adjList.get(current.idx).size(); i++) {
-				Node next = adjList.get(current.idx).get(i);
-
-				// 비용이 더 싸다면 변경
-				if (dist[next.idx] > current.cost + next.cost) {
-					dist[next.idx] = current.cost + next.cost;
-					pq.offer(new Node(next.idx, dist[next.idx]));
-				}
-			}
-		}
-		
 		System.out.println(dist[end]);
 	}
 
-	static class Node {
-		int idx, cost;
+	static void dijkstra() {
+		PriorityQueue<Edge> pq = new PriorityQueue<>();
 
-		public Node(int idx, int cost) {
-			this.idx = idx;
-			this.cost = cost;
+		Arrays.fill(dist, Integer.MAX_VALUE);
+		dist[start] = 0;
+		pq.offer(new Edge(start, 0));
+
+		while (!pq.isEmpty()) {
+			Edge current = pq.poll();
+
+			if (visited[current.to]) {
+				continue;
+			}
+
+			visited[current.to] = true;
+
+			for (Edge next : adjList[current.to]) {
+				if (dist[next.to] > next.cost + current.cost) {
+					dist[next.to] = next.cost + current.cost;
+					pq.offer(new Edge(next.to, dist[next.to]));
+				}
+			}
 		}
+
 	}
 
+	static class Edge implements Comparable<Edge> {
+		int to, cost;
+
+		public Edge(int to, int cost) {
+			this.to = to;
+			this.cost = cost;
+		}
+
+		@Override
+		public int compareTo(Edge o) {
+			return cost - o.cost;
+		}
+
+	}
 }
