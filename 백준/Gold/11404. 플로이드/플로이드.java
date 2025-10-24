900455ed4e57b0be916a33dd266068b74056cdf2
jieunlee99 <105425346+jieunlee99@users.noreply.github.com>
2025-10-24 16:46:03 +0900

    [Gold IV] Title: 플로이드, Time: 364 ms, Memory: 42488 KB -BaekjoonHub

--- 백준/Gold/11404. 플로이드/플로이드.java
+++ 백준/Gold/11404. 플로이드/플로이드.java
@@ -1,22 +1,23 @@
-import java.util.*;
+
 import java.io.*;
+import java.util.*;
 
 public class Main {
 
-	static final int INF = 1000000000;
+	static final int INF = 1_000_000_000;
 	static int N, M;
-	static int[][] cost;
+	static int[][] dist;
 
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		N = Integer.parseInt(br.readLine());
-		M = Integer.parseInt(br.readLine());
+		N = Integer.parseInt(br.readLine()); // 도시 수
+		M = Integer.parseInt(br.readLine()); // 버스 수
 
-		cost = new int[N + 1][N + 1];
+		dist = new int[N + 1][N + 1];
 		for (int i = 1; i <= N; i++) {
-			Arrays.fill(cost[i], INF);
-			cost[i][i] = 0;
+			Arrays.fill(dist[i], INF);
+			dist[i][i] = 0;
 		}
 
 		for (int i = 0; i < M; i++) {
@@ -24,18 +25,15 @@ public class Main {
 			int a = Integer.parseInt(st.nextToken());
 			int b = Integer.parseInt(st.nextToken());
 			int c = Integer.parseInt(st.nextToken());
-			cost[a][b] = Math.min(cost[a][b], c);
+			dist[a][b] = Math.min(dist[a][b], c);
 		}
 
-		floyd();
-		
+		fw();
+
 		StringBuilder sb = new StringBuilder();
-		for(int i=1; i<=N; i++) {
-			for(int j=1; j<=N; j++) {
-				if(cost[i][j] == INF) {
-					cost[i][j] = 0;
-				}
-				sb.append(cost[i][j]).append(" ");
+		for (int i = 1; i <= N; i++) {
+			for (int j = 1; j <= N; j++) {
+				sb.append(dist[i][j] == INF ? 0 : dist[i][j]).append(" ");
 			}
 			sb.append("\n");
 		}
@@ -43,12 +41,12 @@ public class Main {
 		System.out.print(sb);
 	}
 
-	static void floyd() {
+	static void fw() {
 		for (int k = 1; k <= N; k++) {
 			for (int i = 1; i <= N; i++) {
 				for (int j = 1; j <= N; j++) {
-					if (cost[i][k] != Integer.MAX_VALUE && cost[k][j] != Integer.MAX_VALUE) {
-						cost[i][j] = Math.min(cost[i][j], cost[i][k] + cost[k][j]);
+					if (dist[i][k] != INF && dist[k][j] != INF) {
+						dist[i][j] = Math.min(dist[i][j], dist[i][k] + dist[k][j]);
 					}
 				}
 			}
