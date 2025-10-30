b7b3070e3eb52cac99c9629b3412c8cebb12da8d
jieunlee99 <105425346+jieunlee99@users.noreply.github.com>
2025-10-30 15:50:26 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 14.01 ms, Memory: 73.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String s) {
+        int min = Arrays.stream(s.split(" ")).mapToInt(Integer::parseInt).min().getAsInt();
+        int max = Arrays.stream(s.split(" ")).mapToInt(Integer::parseInt).max().getAsInt();
+        
+        return min + " " + max;
+    }
+}
