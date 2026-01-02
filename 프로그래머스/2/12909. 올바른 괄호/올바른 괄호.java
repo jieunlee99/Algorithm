8a4bb1b79ed2820d25623d606c1db3c35cdd0405
jieunlee99 <105425346+jieunlee99@users.noreply.github.com>
2026-01-02 16:28:38 +0900

    [level 2] Title: 올바른 괄호, Time: 18.80 ms, Memory: 54.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+class Solution {
+    boolean solution(String s) {
+        Stack<Character> stack = new Stack<>();
+        
+        for(int i=0; i<s.length(); i++) {
+            char c = s.charAt(i);
+            
+            if(c == '(') {
+                stack.push(c);
+            } else if (c == ')') {
+                if(stack.isEmpty()) {
+                    return false;
+                }
+                
+                if(stack.peek() == '(') {
+                    stack.pop();
+                } else {
+                    return false;
+                }
+            }
+        }
+        
+         if(stack.isEmpty()) {
+            return true;
+        }
+        return false;
+    }
+}
