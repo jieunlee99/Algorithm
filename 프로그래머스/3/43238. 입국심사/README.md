f47b19b9601a7b11da0b2a432a554c53784cff66
jieunlee99 <105425346+jieunlee99@users.noreply.github.com>
2026-02-02 15:44:02 +0900

    [level 3] Title: 입국심사, Time: 95.84 ms, Memory: 97.3 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/README.md
+++ 프로그래머스/3/43238. 입국심사/README.md
@@ -1,10 +1,10 @@
 # [level 3] 입국심사 - 43238 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43238) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43238?language=java) 
 
 ### 성능 요약
 
-메모리: 98.8 MB, 시간: 118.98 ms
+메모리: 97.3 MB, 시간: 95.84 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 11월 04일 17:45:50
+2026년 02월 02일 15:44:00
 
 ### 문제 설명
 
--- 프로그래머스/3/43238. 입국심사/입국심사.java
+++ 프로그래머스/3/43238. 입국심사/입국심사.java
@@ -2,14 +2,14 @@ import java.util.*;
 
 class Solution {
     public long solution(int n, int[] times) {
-        Arrays.sort(times); // 빠른 순 정렬
+        Arrays.sort(times);
         
-        long left = 1; // 최소 1명, 최소 1분일 때
-        long right = (long)times[times.length-1]*n; // 모든 사람이 제일 오래 기다리는 줄에 섰을 때
+        long left = 0;
+        long right = (long)times[times.length-1]*n; 
         
         long answer = right;
         
-        while(left<=right) {
+        while(left <= right) {
             long sum = 0;
             long mid = (left+right)/2;
             
@@ -17,7 +17,7 @@ class Solution {
                 sum += mid/time;
             }
             
-            if(sum >= n) { // n명이 1분 이상 기다려야 하기 때문
+            if(sum >= n) {
                 answer = mid;
                 right = mid-1;
             } else {
