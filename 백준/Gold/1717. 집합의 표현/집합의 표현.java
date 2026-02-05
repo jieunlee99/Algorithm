f45c65b30513599dc9bfb7752285785459fdfe5b
jieunlee99 <105425346+jieunlee99@users.noreply.github.com>
2026-02-05 17:43:39 +0900

    [Gold V] Title: 집합의 표현, Time: 408 ms, Memory: 50616 KB -BaekjoonHub

--- 백준/Gold/1717. 집합의 표현/집합의 표현.java
+++ 백준/Gold/1717. 집합의 표현/집합의 표현.java
@@ -5,63 +5,54 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
+    static int[] parent;
 
-	static int N, M;
-	static int[] parent;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
 
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken()); // 초기 n+1개의 집합: {0}, {1}, ..., {n}
-		M = Integer.parseInt(st.nextToken()); // 연산의 개수
+        parent = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            parent[i] = i;
+        }
 
-		parent = new int[N + 1];
-		for (int i = 0; i <= N; i++) {
-			parent[i] = i;
-		}
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int type = Integer.parseInt(st.nextToken());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
 
-		while (M-- > 0) {
-			st = new StringTokenizer(br.readLine());
-			int type = Integer.parseInt(st.nextToken());
-			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
+            if (type == 0) {
+                union(a, b);
+            } else if (type == 1) {
+                if (find(a) == find(b)) {
+                    sb.append("YES").append("\n");
+                } else {
+                    sb.append("NO").append("\n");
+                }
+            }
+        }
 
-			// 합집합
-			if (type == 0) {
-				union(a, b);
-			}
+        System.out.print(sb);
+    }
 
-			// a와 b가 같은 집합에 속하는지 확인 - YES/NO
-			else if (type == 1) {
-				if (find(a) == find(b)) {
-					sb.append("YES").append("\n");
-				} else {
-					sb.append("NO").append("\n");
-				}
-			}
-		}
+    public static void union(int x, int y) {
+        int rootX = find(x);
+        int rootY = find(y);
+        if (rootX != rootY) {
+            parent[rootY] = rootX;
+        }
+    }
 
-		System.out.println(sb);
-	}
-
-	// 합집합 수행
-	static void union(int a, int b) {
-		int aRoot = find(a);
-		int bRoot = find(b);
-
-		if (aRoot != bRoot) {
-			parent[aRoot] = bRoot;
-		}
-	}
-
-	// 해당 원소의 root를 찾아줌
-	static int find(int num) {
-		if (parent[num] != num) {
-			parent[num] = find(parent[num]); // 경로 압축
-		}
-		return parent[num];
-	}
+    public static int find(int x) {
+        if (parent[x] == x) {
+            return x;
+        }
+        return parent[x] = find(parent[x]);
+    }
 
 }
