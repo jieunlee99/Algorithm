300e69dbfd93e4830727ef22c7564978712c4ad3
jieunlee99 <105425346+jieunlee99@users.noreply.github.com>
2026-02-09 15:19:28 +0900

    [Gold III] Title: LCA, Time: 384 ms, Memory: 43572 KB -BaekjoonHub

--- 백준/Gold/11437. LCA/LCA.java
+++ 백준/Gold/11437. LCA/LCA.java
@@ -1,120 +1,112 @@
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
 
-	static final int K = 16;
-	static int N, M;
-	static boolean[] visited;
-	static int[] depth;
-	static int[][] parent;
-	static List<Integer>[] adjList;
+    static final int K = 16;
+    static int N, M;
+    static boolean[] visited;
+    static int[] depth;
+    static int[][] parent;
+    static List<Integer>[] adjList;
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		N = Integer.parseInt(br.readLine());
+        N = Integer.parseInt(br.readLine());
 
-		depth = new int[N + 1];
-		visited = new boolean[N + 1];
-		parent = new int[K + 1][N + 1];
-		adjList = new ArrayList[N + 1];
-		for (int i = 1; i <= N; i++) {
-			adjList[i] = new ArrayList<>();
-		}
+        depth = new int[N + 1];
+        visited = new boolean[N + 1];
+        parent = new int[K + 1][N + 1];
 
-		for (int i = 0; i < N - 1; i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
-			adjList[a].add(b);
-			adjList[b].add(a);
-		}
+        adjList = new ArrayList[N + 1];
+        for (int i = 1; i <= N; i++) {
+            adjList[i] = new ArrayList<>();
+        }
 
-		bfs(1);
-		initSparseTree();
+        for (int i = 0; i < N - 1; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            adjList[a].add(b);
+            adjList[b].add(a);
+        }
 
-		M = Integer.parseInt(br.readLine());
+        bfs(1); // 루트는 1번
+        initSparseTree(); // 족보 완성
 
-		StringBuilder sb = new StringBuilder();
-		for (int i = 0; i < M; i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
-			sb.append(lca(a, b)).append("\n");
-		}
+        M = Integer.parseInt(br.readLine());
 
-		System.out.println(sb);
-	}
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < M; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            sb.append(lca(a, b)).append("\n");
+        }
 
-	// 각 노드의 깊이와 직계 부모 설정 완료
-	static void bfs(int root) {
-		Queue<Integer> queue = new LinkedList<>();
+        System.out.print(sb);
+    }
 
-		depth[root] = 0;
-		visited[root] = true;
-		queue.offer(root);
+    static int lca(int a, int b) {
+        if (depth[a] > depth[b]) {
+            int temp = a;
+            a = b;
+            b = temp;
+        }
 
-		while (!queue.isEmpty()) {
-			int current = queue.poll();
+        // b를 a와 같은 깊이에 있도록 올려줌
+        for (int k = K; k >= 0; k--) {
+            if ((depth[b] - depth[a]) >= (1 << k)) {
+                b = parent[k][b];
+            }
+        }
 
-			for (int next : adjList[current]) {
-				if (!visited[next]) {
-					visited[next] = true;
-					depth[next] = depth[current] + 1;
-					parent[0][next] = current;
-					queue.offer(next);
-				}
-			}
-		}
+        // 같다면 바로 리턴
+        if (a == b) {
+            return a;
+        }
 
-	}
+        // 둘이 같아지기 직전까지 올려줌
+        for (int k = K; k >= 0; k--) {
+            if (parent[k][a] != parent[k][b]) {
+                a = parent[k][a];
+                b = parent[k][b];
+            }
+        }
 
-	// 족보 완성
-	static void initSparseTree() {
-		for (int k = 1; k <= K; k++) {
-			for (int v = 1; v <= N; v++) {
-				parent[k][v] = parent[k - 1][parent[k - 1][v]];
-			}
-		}
-	}
+        // 같아지기 직전의 부모가 lca가 됨
+        return parent[0][a];
+    }
 
-	static int lca(int a, int b) {
-		// 항상 b가 더 크도록 한다.
-		if (depth[a] > depth[b]) {
-			int temp = a;
-			a = b;
-			b = temp;
-		}
+    static void initSparseTree() {
+        for (int k = 1; k <= K; k++) {
+            for (int v = 1; v <= N; v++) {
+                parent[k][v] = parent[k - 1][parent[k - 1][v]];
+            }
+        }
+    }
 
-		// b를 a와 같은 깊이에 있도록 올려준다.
-		for (int k = K; k >= 0; k--) {
-			if ((depth[b] - depth[a]) >= (1 << k)) {
-				b = parent[k][b];
-			}
-		}
+    static void bfs(int root) {
+        Queue<Integer> queue = new LinkedList<>();
 
-		// 같다면 바로 리턴
-		if (a == b) {
-			return a;
-		}
+        depth[root] = 0;
+        visited[root] = true;
+        queue.offer(root);
 
-		// 둘이 같아지기 직전까지 올림
-		for (int k = K; k >= 0; k--) {
-			if (parent[k][a] != parent[k][b]) {
-				a = parent[k][a];
-				b = parent[k][b];
-			}
-		}
+        while (!queue.isEmpty()) {
+            int current = queue.poll();
 
-		// 같아지기 직전의 부모가 lca
-		return parent[0][a];
-	}
+            for (int next : adjList[current]) {
+                if (!visited[next]) {
+                    visited[next] = true;
+                    depth[next] = depth[current] + 1;
+                    parent[0][next] = current;
+                    queue.offer(next);
+                }
+            }
+        }
+    }
 }
