5295c826a8b5974407e3f6622ca57e8386448374
jieunlee99 <105425346+jieunlee99@users.noreply.github.com>
2026-02-13 18:04:47 +0900

    [Platinum III] Title: 교수님은 기다리지 않는다, Time: 1244 ms, Memory: 312132 KB -BaekjoonHub

--- 백준/Platinum/3830. 교수님은 기다리지 않는다/교수님은 기다리지 않는다.java
+++ 백준/Platinum/3830. 교수님은 기다리지 않는다/교수님은 기다리지 않는다.java
@@ -1,74 +1,72 @@
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
 
-	static int N, M;
-	static int[] parent;
-	static int[] weight;
+    static int N, M;
+    static int[] parent;
+    static int[] weight;
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
 
-		while (true) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			N = Integer.parseInt(st.nextToken()); // 샘플의 수
-			M = Integer.parseInt(st.nextToken()); // 일의 수
-			if (N == 0 && M == 0) {
-				break;
-			}
+        while (true) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            N = Integer.parseInt(st.nextToken());
+            M = Integer.parseInt(st.nextToken());
+            if (N == 0 && M == 0) {
+                break;
+            }
 
-			parent = new int[N + 1];
-			for (int i = 1; i <= N; i++) {
-				parent[i] = i;
-			}
+            parent = new int[N + 1];
+            for (int i = 1; i <= N; i++) {
+                parent[i] = i;
+            }
 
-			weight = new int[N + 1];
+            weight = new int[N + 1];
 
-			for (int i = 0; i < M; i++) {
-				st = new StringTokenizer(br.readLine());
-				String type = st.nextToken();
-				int a = Integer.parseInt(st.nextToken());
-				int b = Integer.parseInt(st.nextToken());
+            for (int i = 0; i < M; i++) {
+                st = new StringTokenizer(br.readLine());
+                String type = st.nextToken();
+                int a = Integer.parseInt(st.nextToken());
+                int b = Integer.parseInt(st.nextToken());
 
-				if (type.equals("!")) {
-					int w = Integer.parseInt(st.nextToken());
-					union(a, b, w);
-				} else if (type.equals("?")) {
-					if (find(a) == find(b)) {
-						sb.append(weight[b] - weight[a]).append("\n");
-					} else {
-						sb.append("UNKNOWN").append("\n");
-					}
-				}
-			}
-		}
+                if (type.equals("!")) {
+                    int w = Integer.parseInt(st.nextToken());
+                    union(a, b, w);
+                } else if (type.equals("?")) {
+                    if (find(a) == find(b)) {
+                        sb.append(weight[b] - weight[a]).append("\n");
+                    } else {
+                        sb.append("UNKNOWN").append("\n");
+                    }
+                }
+            }
+        }
+        System.out.print(sb);
+    }
 
-		System.out.println(sb);
+    static int find(int x) {
+        if (parent[x] == x) {
+            return x;
+        }
 
-	}
+        int p = find(parent[x]);
+        weight[x] += weight[parent[x]];
+        return parent[x] = p;
+    }
 
-	static int find(int num) {
-		if (parent[num] == num) {
-			return num;
-		}
+    static void union(int x, int y, int w) {
+        int xRoot = find(x);
+        int yRoot = find(y);
 
-		int p = find(parent[num]);
-		weight[num] += weight[parent[num]];
-		return parent[num] = p;
-	}
-
-	static void union(int a, int b, int w) {
-		int aRoot = find(a);
-		int bRoot = find(b);
-
-		if (aRoot != bRoot) {
-			parent[bRoot] = aRoot;
-			weight[bRoot] = weight[a] + w - weight[b];
-		}
-	}
+        if (xRoot != yRoot) {
+            parent[yRoot] = xRoot;
+            weight[yRoot] = weight[x] + w - weight[y];
+        }
+    }
 }
