2200f576f4b70b4761368f6a1b74fa6844c0361b
jieunlee99 <105425346+jieunlee99@users.noreply.github.com>
2026-02-14 16:25:30 +0900

    [Gold IV] Title: 타임머신, Time: 252 ms, Memory: 19824 KB -BaekjoonHub

--- 백준/Gold/11657. 타임머신/타임머신.java
+++ 백준/Gold/11657. 타임머신/타임머신.java
@@ -1,83 +1,86 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.StringTokenizer;
 
 public class Main {
 
-	static int N, M;
+    static final long INF = Long.MAX_VALUE;
+    static int N, M;
+    static long[] dist;
+    static List<Edge> edges;
 
-	static long[] dist;
-	static ArrayList<Edge> edges;
+    static class Edge {
+        int u, v, w;
 
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
+        public Edge(int u, int v, int w) {
+            this.u = u;
+            this.v = v;
+            this.w = w;
+        }
+    }
 
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		dist = new long[N+1];
-		edges = new ArrayList<>();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
 
-		for (int i = 0; i < M; i++) {
-			st = new StringTokenizer(br.readLine());
-			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
-			int c = Integer.parseInt(st.nextToken());
+        dist = new long[N + 1];
+        edges = new ArrayList<>();
 
-			edges.add(new Edge(a, b, c));
-		}
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+            int C = Integer.parseInt(st.nextToken());
+            edges.add(new Edge(A, B, C));
+        }
 
-		boolean hasNegativeCycle = bellmanFord();
+        StringBuilder sb = new StringBuilder();
+        boolean hasNegativeCycle = bellmanFord();
 
-		if (hasNegativeCycle) {
-			sb.append(-1).append("\n");
-		} else {
-			for (int i = 2; i <= N; i++) {
-				if (dist[i] == Long.MAX_VALUE) {
-					sb.append(-1).append("\n");
-				} else {
-					sb.append(dist[i]).append("\n");
-				}
-			}
-		}
+        if (hasNegativeCycle) {
+            System.out.println("-1");
+        } else {
+            for (int i = 2; i <= N; i++) {
+                // 경로가 없는 경우 (INF 그대로인 경우) -1 출력
+                if (dist[i] == INF) {
+                    sb.append("-1").append("\n");
+                } else {
+                    sb.append(dist[i]).append("\n");
+                }
+            }
+            System.out.print(sb);
+        }
+    }
 
-		System.out.print(sb.toString());
-	}
+    static boolean bellmanFord() {
+        Arrays.fill(dist, INF);
+        dist[1] = 0;
 
-	static boolean bellmanFord() {
-		Arrays.fill(dist, Long.MAX_VALUE);
+        for (int i = 0; i < N - 1; i++) {
 
-		dist[1] = 0;
+            for (Edge edge : edges) {
+                if (dist[edge.u] == INF) {
+                    continue;
+                }
 
-		for (int i = 0; i < N - 1; i++) {
-			for (Edge edge : edges) {
-				if (dist[edge.from] == Long.MAX_VALUE) {
-					continue;
-				}
+                if (dist[edge.v] > dist[edge.u] + edge.w) {
+                    dist[edge.v] = dist[edge.u] + edge.w;
+                }
+            }
+        }
 
-				if (dist[edge.to] > dist[edge.from] + edge.cost) {
-					dist[edge.to] = dist[edge.from] + edge.cost;
-				}
-			}
-		}
-
-		for (Edge edge : edges) {
-			if (dist[edge.from] != Long.MAX_VALUE && dist[edge.to] > dist[edge.from] + edge.cost) {
-				return true;
-			}
-		}
-		return false;
-	}
-}
-
-class Edge {
-	int from, to, cost;
-
-	public Edge(int from, int to, int cost) {
-		this.from = from;
-		this.to = to;
-		this.cost = cost;
-	}
-
+        for (Edge edge : edges) {
+            if (dist[edge.u] != INF && dist[edge.v] > dist[edge.u] + edge.w) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
