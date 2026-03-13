716b2f1f34da0e2179be9ed903a0cf77bfd06c7d
jieunlee99 <105425346+jieunlee99@users.noreply.github.com>
2026-03-13 17:55:57 +0900

    [level 2] Title: 소수 찾기, Time: 30.08 ms, Memory: 93.8 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -5,36 +5,33 @@ class Solution {
     
     public int solution(String numbers) {
         int answer = 0;
-        
-        // 재귀 통해서 만들어질 수 있는 숫자들을 모두 set에 추가
-        recursive("", numbers); 
-        
-        // set에서 소수만 찾아 개수 세기
+        recursive("", numbers);
         for(int num:set) {
             if(isPrime(num)) {
                 answer++;
             }
         }
-        
         return answer;
     }
     
-    public void recursive(String comb, String others) {
-        // 빈 문자열이 아니라면 set에 추가
-        if(!comb.equals("")) {
-            set.add(Integer.parseInt(comb));
+    public void recursive(String current, String others) {
+        // 만들어진 문자열을 set에 추가함
+        if(!current.equals("")) {
+            set.add(Integer.parseInt(current));
         }
         
-        // 남은 글자 중 하나를 골라 새로운 조합 만들기 -> 재귀
+        // 현재 문자열에 캐릭터 하나를 추가하고
+        // 남은 문자열에서 해당 캐릭터를 제거함
         for(int i=0; i<others.length(); i++) {
-            recursive(comb+others.charAt(i), others.substring(0, i)+others.substring(i+1));
+            recursive(current+others.charAt(i), others.substring(0, i)+others.substring(i+1));
         }
     }
     
-    public boolean isPrime(int num) {
-        if(num < 2) return false;
-        for(int i=2; i<=(int)Math.sqrt(num); i++) {
-            if(num%i == 0) {
+    // 소수인지 확인
+    public boolean isPrime(int x) {
+        if(x < 2) return false;
+        for(int i=2; i<=(int)Math.sqrt(x); i++) {
+            if(x%i == 0) {
                 return false;
             }
         }
