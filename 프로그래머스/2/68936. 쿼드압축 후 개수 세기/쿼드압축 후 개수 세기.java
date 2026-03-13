2f31ea1da96f10e1101fc2cf2d03de6b3c19e71e
jieunlee99 <105425346+jieunlee99@users.noreply.github.com>
2026-03-13 17:27:28 +0900

    [level 2] Title: 쿼드압축 후 개수 세기, Time: 8.90 ms, Memory: 127 MB -BaekjoonHub

--- 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.java
+++ 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.java
@@ -1,14 +1,9 @@
 class Solution {
     
-    boolean[][] visited;
-    
     int[] answer = {0, 0};
     
     public int[] solution(int[][] arr) {
-        
-        int n = arr.length;
-        visited = new boolean[n][n];
-        dfs(arr, 0, 0, n);
+        dfs(arr, 0, 0, arr.length);
         return answer;
     }
     
@@ -30,11 +25,6 @@ class Solution {
         
         if(canCompress(arr, x, y, size)) {
             int num = arr[x][y];
-            for(int i=x; i<x+size; i++) {
-                for(int j=y; j<y+size; j++) {
-                    visited[i][j] = true;
-                }
-            }
             answer[num] += 1;
             return;
         }
