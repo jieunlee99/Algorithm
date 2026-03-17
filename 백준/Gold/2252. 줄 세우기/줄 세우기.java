ed74ecf6324b049e033399313b5eb5d216eeb6e3
jieunlee99 <105425346+jieunlee99@users.noreply.github.com>
2026-03-17 15:39:21 +0900

    [Gold III] Title: 줄 세우기, Time: 424 ms, Memory: 46812 KB -BaekjoonHub

--- 백준/Gold/2252. 줄 세우기/줄 세우기.java
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.java
@@ -1,71 +1,61 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
 
-	static int N, M;
-	static int[] inDegree;
-	static List<Integer>[] graph;
-	static StringBuilder sb = new StringBuilder();
+    static int N, M;
+    static int[] indegree;
+    static List<Integer>[] graph;
+    static StringBuilder sb = new StringBuilder();
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken()); // 학생 수
-		M = Integer.parseInt(st.nextToken()); // 키를 비교한 횟수
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
 
-		inDegree = new int[N + 1];
-		graph = new ArrayList[N + 1];
-		for (int i = 1; i <= N; i++) {
-			graph[i] = new ArrayList<>();
-		}
+        indegree = new int[N + 1];
+        graph = new ArrayList[N + 1];
+        for (int i = 1; i <= N; i++) {
+            graph[i] = new ArrayList<>();
+        }
 
-		while (M-- > 0) {
-			st = new StringTokenizer(br.readLine());
-			// a가 b보다 앞에 서야 한다는 의미
-			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
+        while (M-- > 0) {
+            st = new StringTokenizer(br.readLine());
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+            graph[A].add(B);
+            indegree[B]++;
+        }
 
-			graph[a].add(b); // a->b
-			inDegree[b]++;
-		}
+        topologicalSort();
 
-		topologicalSort();
+        System.out.println(sb);
+    }
 
-		System.out.println(sb);
-	}
+    private static void topologicalSort() {
+        Queue<Integer> queue = new LinkedList<>();
 
-	private static void topologicalSort() {
-		Queue<Integer> queue = new LinkedList<>();
+        for (int i = 1; i <= N; i++) {
+            if (indegree[i] == 0) {
+                queue.offer(i);
+            }
+        }
 
-		// 진입차수가 0인 노드를 큐에 삽입함
-		for (int i = 1; i <= N; i++) {
-			if (inDegree[i] == 0) {
-				queue.offer(i);
-			}
-		}
+        while (!queue.isEmpty()) {
+            int current = queue.poll();
+            sb.append(current).append(" ");
 
-		while (!queue.isEmpty()) {
-			int current = queue.poll();
-			sb.append(current).append(" ");
+            for (int next : graph[current]) {
+                indegree[next]--;
 
-			// 현재 노드와 연결된 노드들의 진입 차수를 감소시킴
-			for (int next : graph[current]) {
-				inDegree[next]--;
-				
-				// 진입차수가 0이 되면 큐에 삽입
-				if(inDegree[next] == 0) {
-					queue.offer(next);
-				}
-			}
-		}
-	}
-
+                if (indegree[next] == 0) {
+                    queue.offer(next);
+                }
+            }
+        }
+    }
 }
