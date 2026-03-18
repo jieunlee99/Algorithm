3cda1d70bf1f922e844436900f7bc7ca9f26eaff
jieunlee99 <105425346+jieunlee99@users.noreply.github.com>
2026-03-18 13:26:56 +0900

    [Gold IV] Title: 행성 연결, Time: 536 ms, Memory: 95896 KB -BaekjoonHub

--- 백준/Gold/16398. 행성 연결/행성 연결.java
+++ 백준/Gold/16398. 행성 연결/행성 연결.java
@@ -1,87 +1,48 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-
-    static int N;
-    static int[] parent;
-    static List<Edge> edges;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        N = Integer.parseInt(br.readLine());
-
-        parent = new int[N];
-        for (int i = 0; i < N; i++) {
-            parent[i] = i;
-        }
-        edges = new ArrayList<>();
-
-
-        for (int i = 0; i < N; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < N; j++) {
-                int cost = Integer.parseInt(st.nextToken());
-                edges.add(new Edge(i, j, cost));
+        StringTokenizer st; 
+        
+        int N = Integer.parseInt(br.readLine());
+        int[][] cost = new int[N][N];
+        
+        for(int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for(int j = 0; j < N; j++) {
+                cost[i][j] = Integer.parseInt(st.nextToken());
             }
         }
-
-        Collections.sort(edges);
-
-        int depth = 1;
-        long totalCost = 0;
-
-        for (Edge edge : edges) {
-            if (union(edge.u, edge.v)) {
-                totalCost += edge.cost;
-                depth++;
-
-                if (depth == N) {
-                    break;
+        
+        boolean[] visited = new boolean[N];
+        long[] minEdge = new long[N];
+        Arrays.fill(minEdge, Long.MAX_VALUE);
+        
+        minEdge[0] = 0;
+        long result = 0;
+        
+        for(int i = 0; i < N; i++) {
+            int minVertex = -1;
+            long min = Long.MAX_VALUE;
+            
+            for(int j = 0; j < N; j++) {
+                if(!visited[j] && minEdge[j] < min) {
+                    min = minEdge[j];
+                    minVertex = j;
+                }
+            }
+            
+            visited[minVertex] = true;
+            result += min;
+            
+            for(int j = 0; j < N; j++) {
+                if(!visited[j] && cost[minVertex][j] < minEdge[j]) {
+                    minEdge[j] = cost[minVertex][j];
                 }
             }
         }
-
-        System.out.println(totalCost);
-    }
-
-    static int find(int x) {
-        if (parent[x] == x) {
-            return x;
-        }
-        return parent[x] = find(parent[x]);
-    }
-
-    static boolean union(int x, int y) {
-        int xRoot = find(x);
-        int yRoot = find(y);
-
-        if (xRoot != yRoot) {
-            parent[yRoot] = xRoot;
-            return true;
-        }
-
-        return false;
-    }
-
-    static class Edge implements Comparable<Edge> {
-        int u, v, cost;
-
-        public Edge(int u, int v, int cost) {
-            this.u = u;
-            this.v = v;
-            this.cost = cost;
-        }
-
-        @Override
-        public int compareTo(Edge o) {
-            return this.cost - o.cost;
-        }
+        System.out.println(result);
     }
 }
