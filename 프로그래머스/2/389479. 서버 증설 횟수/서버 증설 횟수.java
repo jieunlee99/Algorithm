ca3e6ed6ba6a28dcb18fa021a6ce343cab788c0c
jieunlee99 <105425346+jieunlee99@users.noreply.github.com>
2026-03-26 15:48:29 +0900

    [level 2] Title: 서버 증설 횟수, Time: 0.16 ms, Memory: 85.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/389479. 서버 증설 횟수/서버 증설 횟수.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] players, int m, int k) {
+        int answer = 0;
+        
+        Queue<Integer> queue = new LinkedList<>();
+        int current = 0; 
+        
+        for (int player : players) {
+            // k시간마다 서버 정리
+            if (queue.size() == k) {
+                current -= queue.poll();
+            }
+            
+            int required = player / m;
+            
+            // 서버가 부족하다면 부족한 만큼 증설
+            if (required > current) {
+                int need = required - current;
+                queue.offer(need);       
+                current += need;  
+                answer += need;         
+            } else {
+                queue.offer(0);
+            }
+        }
+        
+        return answer;
+    }
+}
