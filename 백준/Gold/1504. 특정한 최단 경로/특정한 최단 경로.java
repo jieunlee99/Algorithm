2178f7e3786dc336ecde0b7192ed12b17902117b
jieunlee99 <105425346+jieunlee99@users.noreply.github.com>
2026-04-04 22:08:37 +0900

    [Gold IV] Title: 특정한 최단 경로, Time: 636 ms, Memory: 68668 KB -BaekjoonHub

--- 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.java
+++ 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.java
@@ -1,109 +1,94 @@
-import java.io.*;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
 
-	static int N, E;
+    static class Edge implements Comparable<Edge> {
+        int to, dist;
 
-	static ArrayList<Edge>[] adjList;
-	static boolean[] visited;
-	static int[] dist;
+        public Edge(int to, int dist) {
+            this.to = to;
+            this.dist = dist;
+        }
 
-	static final int INF = 200_000 * 1_000;
+        @Override
+        public int compareTo(Edge e) {
+            return this.dist - e.dist;
+        }
+    }
 
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static List<Edge>[] adjList;
+    static boolean[] visited;
+    static int[] dist;
+    static final int INF = 200_000 * 1_000;
 
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
-		E = Integer.parseInt(st.nextToken());
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		adjList = new ArrayList[N + 1];
-		for (int i = 1; i <= N; i++) {
-			adjList[i] = new ArrayList<>();
-		}
-		visited = new boolean[N + 1];
-		dist = new int[N + 1];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int E = Integer.parseInt(st.nextToken());
 
-		for (int i = 0; i < E; i++) {
-			st = new StringTokenizer(br.readLine());
-			int u = Integer.parseInt(st.nextToken());
-			int v = Integer.parseInt(st.nextToken());
-			int dist = Integer.parseInt(st.nextToken());
+        adjList = new ArrayList[N + 1];
+        for (int i = 1; i <= N; i++) {
+            adjList[i] = new ArrayList<>();
+        }
 
-			adjList[u].add(new Edge(v, dist));
-			adjList[v].add(new Edge(u, dist));
-		}
+        visited = new boolean[N + 1];
+        dist = new int[N + 1];
 
-		st = new StringTokenizer(br.readLine());
-		int v1 = Integer.parseInt(st.nextToken());
-		int v2 = Integer.parseInt(st.nextToken());
+        for (int i = 0; i < E; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+            adjList[a].add(new Edge(b, c));
+            adjList[b].add(new Edge(a, c));
+        }
 
-		// (case 1) 1 -> v1 -> v2 -> N
-		int case1 = 0;
-		case1 += dijkstra(1, v1);
-		case1 += dijkstra(v1, v2);
-		case1 += dijkstra(v2, N);
+        st = new StringTokenizer(br.readLine());
+        int v1 = Integer.parseInt(st.nextToken());
+        int v2 = Integer.parseInt(st.nextToken());
 
-		// (case 2) 1 -> v2 -> v1 -> N
-		int case2 = 0;
-		case2 += dijkstra(1, v2);
-		case2 += dijkstra(v2, v1);
-		case2 += dijkstra(v1, N);
+        int cnt1 = dijkstra(1, v1) + dijkstra(v1, v2) + dijkstra(v2, N);
+        int cnt2 = dijkstra(1, v2) + dijkstra(v2, v1) + dijkstra(v1, N);
 
-		if (case1 >= INF && case2 >= INF) {
-			bw.write(-1 + "\n");
-		} else {
-			bw.write(Math.min(case1, case2) + "\n");
-		}
+        if (cnt1 >= INF && cnt2 >= INF) {
+            System.out.println(-1);
+        } else {
+            System.out.println(Math.min(cnt1, cnt2));
+        }
+    }
 
-		bw.flush();
-		br.close();
-		bw.close();
-	}
+    public static int dijkstra(int from, int to) {
+        PriorityQueue<Edge> pq = new PriorityQueue<>();
 
-	static int dijkstra(int from, int to) {
-		PriorityQueue<Edge> pq = new PriorityQueue<>();
+        Arrays.fill(visited, false);
+        Arrays.fill(dist, INF);
 
-		Arrays.fill(visited, false);
-		Arrays.fill(dist, INF);
+        dist[from] = 0;
+        pq.offer(new Edge(from, 0));
 
-		dist[from] = 0;
-		pq.add(new Edge(from, 0));
+        while (!pq.isEmpty()) {
+            Edge current = pq.poll();
 
-		while (!pq.isEmpty()) {
-			Edge current = pq.poll();
+            if (visited[current.to]) {
+                continue;
+            }
 
-			if (visited[current.to]) {
-				continue;
-			}
+            visited[current.to] = true;
 
-			visited[current.to] = true;
+            for (Edge next : adjList[current.to]) {
+                if (dist[next.to] > dist[current.to] + next.dist) {
+                    dist[next.to] = dist[current.to] + next.dist;
+                    pq.offer(new Edge(next.to, dist[next.to]));
+                }
+            }
+        }
 
-			for (Edge next : adjList[current.to]) {
-				if (dist[next.to] > dist[current.to] + next.dist) {
-					dist[next.to] = dist[current.to] + next.dist;
-					pq.add(new Edge(next.to, dist[next.to]));
-				}
-			}
-		}
-
-		return dist[to];
-
-	}
-}
-
-class Edge implements Comparable<Edge> {
-	int to, dist;
-
-	public Edge(int to, int dist) {
-		this.to = to;
-		this.dist = dist;
-	}
-
-	@Override
-	public int compareTo(Edge o) {
-		return dist - o.dist;
-	}
+        return dist[to];
+    }
 }
