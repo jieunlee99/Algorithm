843bc558fc2d45c256d242e13daad9310d23377b
jieunlee99 <105425346+jieunlee99@users.noreply.github.com>
2026-08-28 17:22:49 +0900

    [level 2] Title: 구명보트, Time: 10.63 ms, Memory: 58.6 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.java
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -2,20 +2,21 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] people, int limit) {
+        int answer = 0;
         
         Arrays.sort(people);
         
-        int answer = 0;
-        int left = 0, right = people.length-1;
-        
-        while(left <= right) {
-            // 무거운 사람은 무조건 탑승 => right--
-            // 가벼운 사람은 같이 탈 수 있다면 탑승 => left++
-            if(people[left]+people[right--] <= limit) {
+        int n = people.length;
+                
+        int left = 0;
+        for(int right=n-1; right>=left; right--) {
+            if(people[left]+people[right] <= limit) {
                 left++;
+                answer++;
+            } else {
+                answer++;   
             }
-            answer++;
-        }
+        }   
         
         return answer;
     }
