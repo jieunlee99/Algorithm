f051a22486f31c14521d407d5347e61830ac1d7d
jieunlee99 <105425346+jieunlee99@users.noreply.github.com>
2026-09-01 15:20:06 +0900

    [level 2] Title: H-Index, Time: 5.09 ms, Memory: 72.4 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.java
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -2,35 +2,27 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] citations) {
+        Arrays.sort(citations);
+
         int n = citations.length;
         
-        // 정렬
-        Arrays.sort(citations);
+        int left = 0;
+        int right = n - 1;
         
-        // 이진 탐색
         int answer = 0;
-        int low = 0;
-        int high = n;
-        
-        while(low <= high) {
-            int mid = (low+high)/2;
-            
-            // 모든 논문은 0번이상 인용되기 때문에 검사할 필요 x
-            if(mid == 0) {
-                low = mid+1;
-                continue;
-            }
-            
-            // h번 이상 인용된 논문이 h개 이상인지 확인
-            if(citations[n-mid] >= mid) {
-                answer = mid;
-                low = mid+1;
+
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            int h = n - mid;
+
+            if (citations[mid] >= h) {
+                answer = h;
+                right = mid - 1;
             } else {
-                high = mid-1;
+                left = mid + 1;
             }
         }
-        
-        
+
         return answer;
     }
 }
