23341bd988dc9d4035e49139d915d2fa455b16b0
jieunlee99 <105425346+jieunlee99@users.noreply.github.com>
2026-09-01 14:31:42 +0900

    [level 2] Title: 할인 행사, Time: 20.81 ms, Memory: 124 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.java
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.java
@@ -3,36 +3,47 @@ import java.util.*;
 class Solution {
     public int solution(String[] want, int[] number, String[] discount) {
         int answer = 0;
-        
-        Map<String, Integer> map = new HashMap<>();
-        for(int i = 0; i < want.length; i++) {
-            map.put(want[i], number[i]);
+        int remain = 0;
+
+        Map<String, Integer> need = new HashMap<>();
+
+        for (int i = 0; i < want.length; i++) {
+            need.put(want[i], number[i]);
+            remain += number[i];
         }
-        
-        for(int i = 0; i <= discount.length - 10; i++) {
-            if(isPossibleDay(i, map, discount)) {
+
+        for (int i = 0; i < discount.length; i++) {
+
+            // 새 상품을 윈도우에 추가
+            String in = discount[i];
+
+            if (need.containsKey(in)) {
+                if (need.get(in) > 0) {
+                    remain--;
+                }
+
+                need.put(in, need.get(in) - 1);
+            }
+
+            // 10일을 초과하면 가장 오래된 상품 제거
+            if (i >= 10) {
+                String out = discount[i - 10];
+
+                if (need.containsKey(out)) {
+                    need.put(out, need.get(out) + 1);
+
+                    if (need.get(out) > 0) {
+                        remain++;
+                    }
+                }
+            }
+
+            // 정확히 10일짜리 윈도우가 만들어진 시점부터 검사
+            if (i >= 9 && remain == 0) {
                 answer++;
             }
         }
+
         return answer;
-    }
-    
-    public boolean isPossibleDay(int start, Map<String, Integer> originMap, String[] discount) {
-        Map<String, Integer> copyMap = new HashMap<>(originMap);
-        
-        for(int i = start; i < start + 10; i++) {
-            String item = discount[i];
-            if(copyMap.containsKey(item)) {
-                copyMap.put(item, copyMap.get(item) - 1);
-            }
-        }
-        
-        for(int v : copyMap.values()) {
-            if(v > 0) {
-                return false;
-            }
-        }
-        
-        return true;
     }
 }
