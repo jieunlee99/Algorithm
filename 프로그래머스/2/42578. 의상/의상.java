4405d024c77b658ba1b948bbd169b46a4de88bcc
jieunlee99 <105425346+jieunlee99@users.noreply.github.com>
2026-09-01 15:07:59 +0900

    [level 2] Title: 의상, Time: 0.37 ms, Memory: 74.1 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -5,15 +5,16 @@ class Solution {
         int answer = 1;
         
         Map<String, Integer> map = new HashMap<>();
-        for(String[] cloth:clothes) {
-            map.put(cloth[1], map.getOrDefault(cloth[1], 0)+1);
+        
+        for(int i=0; i<clothes.length; i++) {
+            map.put(clothes[i][1], map.getOrDefault(clothes[i][1], 0)+1);
         }
-
-        Iterator<Integer> iter = map.values().iterator();
-        while (iter.hasNext()) {
-            answer *= iter.next().intValue() + 1;
+        
+        for(int cnt:map.values()) {
+            answer *= cnt+1;
         }
-
-        return answer-1; // 아무 것도 입지 않는 경우 빼기
+        answer -= 1;
+        
+        return answer;
     }
 }
