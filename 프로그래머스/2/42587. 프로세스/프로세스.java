716c4053de7bcaf624b8c69629b268e2f91c3c2f
jieunlee99 <105425346+jieunlee99@users.noreply.github.com>
2026-09-11 15:57:31 +0900

    [level 2] Title: 프로세스, Time: 0.91 ms, Memory: 79.4 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.java
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -1,52 +1,28 @@
 import java.util.*;
 
 class Solution {
-    class Process {
-        int priority;
-        int index;
-        
-        Process(int priority, int index) {
-            this.priority = priority;
-            this.index = index;
-        }
-    }
-    
     public int solution(int[] priorities, int location) {
+        int answer = 0;
         
-        int answer = 0; // index = location 인 프로세스가 몇번째로 실행되는지 찾아야 함
-        
-        // 현재 프로세스 대기 큐
-        Queue<Process> queue = new LinkedList<>();
-        for(int i=0; i<priorities.length; i++) {
-            queue.offer(new Process(priorities[i], i));
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        for(int p:priorities) {
+            pq.offer(p);
         }
         
-        while(!queue.isEmpty()) {
-            Process current = queue.poll();
-            
-            // 현재 프로세스보다 우선순위가 높은 프로세스가 큐 안에 있는지 확인
-            boolean isMax = true;
-            for(Process p:queue) {
-                if(p.priority > current.priority) {
-                    isMax = false;
-                    break;
+        while(!pq.isEmpty()) {
+            for (int i = 0; i < priorities.length; i++) {
+
+                if (pq.peek() == priorities[i]) {
+                    pq.poll();
+                    answer++;
+
+                    if (location == i) {
+                        return answer;
+                    }
                 }
-            }
-            
-            // 현재 프로세스의 우선순위가 가장 높다면 바로 실행
-            if(isMax) {
-                answer++;
-                if(current.index == location) {
-                    return answer;
-                }
-            } 
-            
-            // 우선순위가 더 높은 프로세스가 있다면 현재 프로세스를 맨 뒤로 보냄
-            else {
-                queue.offer(current);
             }
         }
-        
+          
         return answer;
     }
 }
