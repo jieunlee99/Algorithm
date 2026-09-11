7990b8633990f47c2c0508350ece98258bfc6b6c
jieunlee99 <105425346+jieunlee99@users.noreply.github.com>
2026-09-11 18:01:09 +0900

    [level 3] Title: 네트워크, Time: 0.51 ms, Memory: 74.2 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -2,39 +2,42 @@ import java.util.*;
 
 class Solution {
     
-    ArrayList<Integer>[] adjList;
     boolean[] visited;
-    int network;
+    List<Integer>[] adjList;
+    
+    int answer = 0;
     
     public int solution(int n, int[][] computers) {
+        
         visited = new boolean[n];
         
         adjList = new ArrayList[n];
-        for(int i=0; i<n; i++) {
+        for(int i = 0; i < n; i++) {
             adjList[i] = new ArrayList<>();
         }
         
-        for(int i=0; i<n; i++) {
-            for(int j=0; j<n; j++) {
-                if(i != j && computers[i][j] == 1) {
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                if(computers[i][j] == 1) {
                     adjList[i].add(j);
                 }
             }
         }
         
-        for(int i=0; i<n; i++) {
+        for(int i = 0; i < n; i++) {
             if(!visited[i]) {
                 dfs(i);
-                network++;
+                answer++;
             }
         }
         
-        return network;
+        return answer;
     }
     
-    public void dfs(int current) {
-        visited[current] = true;
-        for(int next:adjList[current]) {
+    public void dfs(int start) {
+        visited[start] = true;
+        
+        for(int next : adjList[start]) {
             if(!visited[next]) {
                 dfs(next);
             }
