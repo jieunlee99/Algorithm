7dc604e089c516f6fea3b14e0300889874e75dea
jieunlee99 <105425346+jieunlee99@users.noreply.github.com>
2026-09-23 17:04:41 +0900

    [level 2] Title: 기능개발, Time: 2.62 ms, Memory: 85.1 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -2,35 +2,22 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
-        List<Integer> list = new ArrayList<>();
-
-        int n = progresses.length;
-        int[] arr = new int[n];
-
-        // 각 기능이 완료되기까지 필요한 날짜
-        for (int i = 0; i < n; i++) {
-            arr[i] = (int) Math.ceil((100.0 - progresses[i]) / speeds[i]);
-        }
-
-        int current = arr[0];
-        int cnt = 1;
-
-        for (int i = 1; i < n; i++) {
-            int next = arr[i];
-
-            // 앞 기능보다 먼저 또는 같은 날 끝나면 같이 배포
-            if (current >= next) {
-                cnt++;
+        List<Integer> answerList = new ArrayList<>();
+        
+        int maxDay = 0; 
+        
+        for (int i = 0; i < progresses.length; i++) {
+            int day = (100 - progresses[i] + speeds[i] - 1) / speeds[i];
+            
+            if (day > maxDay) {
+                maxDay = day;
+                answerList.add(1);
             } else {
-                list.add(cnt);
-                cnt = 1;
-                current = next;
+                int lastIdx = answerList.size() - 1;
+                answerList.set(lastIdx, answerList.get(lastIdx) + 1);
             }
         }
-
-        // 마지막 배포 그룹
-        list.add(cnt);
-
-        return list.stream().mapToInt(i -> i).toArray();
+        
+        return answerList.stream().mapToInt(i -> i).toArray();
     }
 }
